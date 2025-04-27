64c328bf00d59ea839c1fc76d175ceb9d94b8fbd
hyeone999 <caps026@naver.com>
2025-04-27 15:54:19 +0900

    Chore: iOS 카카오 네이티브 앱키 환경변수 처리

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import { KAKAO_APP_KEY } from '@env';
 import { initializeKakaoSDK } from '@react-native-kakao/core';
 import { AppRegistry } from 'react-native';
 
@@ -10,6 +11,6 @@ import { name as appName } from './app.json';
 
 // 항상 모든 카카오 API가 불리기 전에 이 함수가 먼저 호출되어야 합니다.
 // 카카오 SDK 초기화
-initializeKakaoSDK('52a96882f6fb7b5a1ba4ae4cd27788c2');
+initializeKakaoSDK(KAKAO_APP_KEY);
 
 AppRegistry.registerComponent(appName, () => App);

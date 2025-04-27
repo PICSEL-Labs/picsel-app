d8e66292caeac3029213e2d1bdccfe190df2d35e
hyeone999 <caps026@naver.com>
2025-04-28 00:37:51 +0900

    Modify: env 불러오는 console.log 삭제

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-import { getKeyHashAndroid } from '@react-native-kakao/core';
 import { login } from '@react-native-kakao/user';
 import { Pressable, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -18,19 +17,19 @@ const TestScreen = () => {
     }
   };
 
-  console.log(getKeyHashAndroid());
-
   // 카카오 response
   console.log(kakaoResult);
 
   return (
     <SafeAreaView className="bg-white flex-1 justify-center items-center">
-      <Text className="text-blue-400">TestScreen</Text>
-
       {/* 카카오 로그인(임시) */}
       <Pressable onPress={signInWithKakao} className="bg-yellow-300 p-4">
         <Text>카카오 로그인</Text>
       </Pressable>
+
+      <Text className="text-black font-bold text-base">
+        kakao res: {kakaoResult}
+      </Text>
     </SafeAreaView>
   );
 };

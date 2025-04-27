3ff6705ebbc84ee96a3c46f4f3c00e354495527a
hyeone999 <caps026@naver.com>
2025-04-27 15:24:07 +0900

    Feat: iOS kakao sdk 초기화

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { initializeKakaoSDK } from '@react-native-kakao/core';
+import { AppRegistry } from 'react-native';
+
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+// 항상 모든 카카오 API가 불리기 전에 이 함수가 먼저 호출되어야 합니다.
+// 카카오 SDK 초기화
+initializeKakaoSDK('52a96882f6fb7b5a1ba4ae4cd27788c2');
 
 AppRegistry.registerComponent(appName, () => App);

f79804d380061f575437a08474394ab6d3e6ef94
hyeone999 <caps026@naver.com>
2025-04-27 18:28:16 +0900

    Modify: 카카오 sdk 초기화 제거

--- index.js
+++ index.js
@@ -2,15 +2,9 @@
  * @format
  */
 
-import { KAKAO_APP_KEY } from '@env';
-import { initializeKakaoSDK } from '@react-native-kakao/core';
 import { AppRegistry } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
-
-// 항상 모든 카카오 API가 불리기 전에 이 함수가 먼저 호출되어야 합니다.
-// 카카오 SDK 초기화
-initializeKakaoSDK(KAKAO_APP_KEY);
 
 AppRegistry.registerComponent(appName, () => App);

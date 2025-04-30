eaa8dd8fa63643351e50a58c03ed9fb14a7c9481
hyeone999 <caps026@naver.com>
2025-04-30 14:54:21 +0900

    Feat: Login API

---
+++ src/shared/lib/api/auth/loginApi.ts
@@ -0,0 +1,15 @@
+import { LoginRequest, LoginResponse } from '@/feature/auth/types';
+import { axiosInstance } from '@/shared/lib/api';
+
+// 로그인 API
+export const loginApi = async ({
+  socialAccessToken,
+  socialType,
+}: LoginRequest): Promise<LoginResponse> => {
+  const response = await axiosInstance.post<LoginResponse>('auth/signin', {
+    socialAccessToken,
+    socialType,
+  });
+
+  return response.data;
+};

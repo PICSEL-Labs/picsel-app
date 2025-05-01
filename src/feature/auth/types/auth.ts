ff3f6e75ddd7dad5b6d8f186b1b384e83a39ba9f
hyeone999 <caps026@naver.com>
2025-05-01 15:09:19 +0900

    Modify: 파일 네이밍 수정

--- src/feature/auth/types/auth.ts
+++ src/feature/auth/types/auth.ts
@@ -1,9 +1,11 @@
 // API
 export type SocialTypes = 'KAKAO' | 'NAVER' | 'GOOGLE' | 'APPLE';
 
+export type SocialLoginFunction = () => Promise<string>;
+
 export interface LoginRequest {
+  socialType: SocialTypes;
   socialAccessToken: string;
-  socialType: string;
 }
 
 export interface LoginResponse {

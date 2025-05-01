9be0236ba99eab6255b893d92f1ed97849fd9427
hyeone999 <caps026@naver.com>
2025-05-01 15:12:48 +0900

    Feat: 서비스 로그인 API 로직 수정 및 구현

--- src/shared/lib/api/auth/loginApi.ts
+++ src/shared/lib/api/auth/loginApi.ts
@@ -1,4 +1,12 @@
-import { LoginRequest, LoginResponse } from '@/feature/auth/types';
+import { login } from '@react-native-kakao/user';
+import NaverLogin from '@react-native-seoul/naver-login';
+
+import {
+  LoginRequest,
+  LoginResponse,
+  SocialLoginFunction,
+  SocialTypes,
+} from '@/feature/auth/types/auth';
 import { axiosInstance } from '@/shared/lib/api';
 
 // 로그인 API
@@ -12,4 +20,26 @@ export const loginApi = async ({
   });
 
   return response.data;
+};
+
+// 소셜 로그인(종합) API
+export const loginStrategies: Record<SocialTypes, SocialLoginFunction> = {
+  KAKAO: async () => {
+    const response = await login(); // Kakao login
+    return response.accessToken;
+  },
+  NAVER: async () => {
+    const response = await NaverLogin.login(); // Naver login
+    return response.successResponse.accessToken;
+  },
+  GOOGLE: async () => {
+    // const data = await login(); // Kakao login
+    // return data.accessToken;
+    return 'test';
+  },
+  APPLE: async () => {
+    // const data = await login(); // Kakao login
+    // return data.accessToken;
+    return 'test';
+  },
 };

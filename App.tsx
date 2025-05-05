fa7a6c7e55045f5dddda152b3e7c557f23c9003c
hyeone999 <caps026@naver.com>
2025-05-05 12:07:03 +0900

    Modify: root 렌더링 수정

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import TestScreen from '@/screens/TestScreen';
+import OnboardingTestScreen from '@/screens/OnboardingTestScreen';
 
 const queryClient = new QueryClient();
 
@@ -18,7 +18,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <TestScreen />
+        <OnboardingTestScreen />
       </SafeAreaProvider>
     </QueryClientProvider>
   );

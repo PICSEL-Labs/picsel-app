b333db89281f5246ed78f31133ad94b1f165bac4
hyeone999 <caps026@naver.com>
2025-05-17 22:15:29 +0900

    Feat: 유저 id 및 닉네임 유효성 검증 API 구현

---
+++ src/feature/auth/signup/api/validateApi.ts
@@ -0,0 +1,18 @@
+import { ValidateResponse, ValidationType } from '../types/types';
+
+import { axiosInstance } from '@/shared/lib/api/axiosInstance';
+
+// 사용자 아이디, 닉네임 유효성 검증 API
+export const validateUserInfoApi = async (
+  type: ValidationType,
+  value: string,
+): Promise<ValidateResponse> => {
+  const response = await axiosInstance.get<ValidateResponse>(
+    `/users/validate/${type}`,
+    {
+      params: { value },
+    },
+  );
+
+  return response.data;
+};

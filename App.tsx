e15d79f420930d2d358b8f65b85cec7cb6ce48c3
hyeone999 <caps026@naver.com>
2025-06-14 09:29:57 +0900

    ✨ Feat :  splash

--- App.tsx
+++ App.tsx
@@ -5,23 +5,33 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 import MainStack from '@/feature/navigation';
 
 const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <NavigationContainer>
-          <MainStack />
-        </NavigationContainer>
+        <GestureHandlerRootView>
+          <NavigationContainer>
+            <MainStack />
+          </NavigationContainer>
+        </GestureHandlerRootView>
       </SafeAreaProvider>
     </QueryClientProvider>
   );

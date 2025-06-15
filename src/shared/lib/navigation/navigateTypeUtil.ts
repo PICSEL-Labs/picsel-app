22eb7c7c3e1cbd16ac3f232203b03a31cdd23043
hyeone999 <caps026@naver.com>
2025-06-15 20:42:40 +0900

    ✨ Feat :  파일 구조 변경

--- src/shared/lib/navigation/navigateTypeUtil.ts
+++
@@ -1,7 +0,0 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
-import { RootStackParamList } from '@/feature/navigation/types';
-
-// ✅ 이 유틸을 통해 어느 스크린이든 쉽게 타입 정의 가능
-export type RootStackNavigationProp =
-  NativeStackNavigationProp<RootStackParamList>;

069a134bfe68ab1cfc0b8479ff37b25aca269804
hyeone999 <caps026@naver.com>
2025-06-21 15:38:17 +0900

    ✨ Feat :  회원가입 route 구현

---
+++ src/navigation/route/signup/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import BrandSearchScreen from '@/screens/signup/brandSearch';
+import NicknameInputScreen from '@/screens/signup/nicknameInput';
+import SelectBrandScreen from '@/screens/signup/selectBrand';
+import SignupSuccessScreen from '@/screens/signup/signupSuccess';
+
+export type SignupNavigationProps = {
+  NicknameInput: undefined;
+  SelectBrand: undefined;
+  BrandSearch: undefined;
+  SignupSuccess: undefined;
+};
+
+const SignupRoute = () => {
+  const Stack = createNativeStackNavigator<SignupNavigationProps>();
+
+  return (
+    <Stack.Navigator id={undefined} screenOptions={{ headerShown: false }}>
+      {/* 닉네임 입력 & 약관동의 화면 */}
+      <Stack.Screen name="NicknameInput" component={NicknameInputScreen} />
+
+      {/* 선호 브랜드 선택 화면 */}
+      <Stack.Screen
+        name="SelectBrand"
+        component={SelectBrandScreen}
+        options={{
+          gestureEnabled: false, // iOS 스와이프 뒤로가기 방지
+        }}
+      />
+
+      {/* 브랜드 검색 화면 */}
+      <Stack.Screen name="BrandSearch" component={BrandSearchScreen} />
+
+      {/* 회원가입 완료 화면 */}
+      <Stack.Screen
+        name="SignupSuccess"
+        component={SignupSuccessScreen}
+        options={{
+          gestureEnabled: false, // iOS 스와이프 뒤로가기 방지
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default SignupRoute;

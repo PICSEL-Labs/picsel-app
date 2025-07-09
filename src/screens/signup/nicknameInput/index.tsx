cc6d6efc097ce6f1029911569ae91c2cc7d6cd1b
hyeone999 <caps026@naver.com>
2025-07-09 13:18:11 +0900

    ✨ Feat :  닉네임 입력 & 약관동의 스크린 구현

--- src/screens/signup/nicknameInput/index.tsx
+++ src/screens/signup/nicknameInput/index.tsx
@@ -1,263 +1,92 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
-import {
-  KeyboardAvoidingView,
-  Platform,
-  Keyboard,
-  Animated,
-  Text,
-  Pressable,
-  View,
-  TextInput,
-} from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
-import TermsModal from '../modal';
-
-import BackIcon from '@/assets/icons/arrow/left-arrow.svg';
-import CheckIcon from '@/assets/icons/checked.svg';
-import CloseIcon from '@/assets/icons/close/icon-close.svg';
-import FailedIcon from '@/assets/icons/nickname-failed.svg';
-import { signupApi } from '@/feature/auth/signup/api/signupApi';
-import { validateUserInfoApi } from '@/feature/auth/signup/api/validateApi';
+import { useButtonService } from '@/feature/auth/signup/model/useButtonService';
+import { useNicknameValidation } from '@/feature/auth/signup/model/useNicknameValidation';
+import { useTermsAgreement } from '@/feature/auth/signup/model/useTermsAgreement';
+import { useUserSignup } from '@/feature/auth/signup/model/useUserSignup';
+import { TermsModal } from '@/feature/auth/signup/ui/modal';
+import NicknameFeedback from '@/feature/auth/signup/ui/organisms/NicknameFeedback';
+import NicknameInput from '@/feature/auth/signup/ui/organisms/NicknameInput';
+import NicknameSubmitButton from '@/feature/auth/signup/ui/organisms/NicknameSubmitButton';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
-import { cn } from '@/shared/lib/cn';
 import { useUserStore } from '@/shared/store';
 import { SignupNavigationProp } from '@/shared/types/navigateTypeUtil';
-import { validateFormat } from '@/shared/utils/validateNickname';
+import SignupHeader from '@/shared/ui/organisms/SignupHeader';
+import SignupIntro from '@/shared/ui/organisms/SignupIntro';
 
 const NicknameInputScreen = () => {
-  const [userNickname, setUserNickname] = useState('');
-  const [focus, setFocus] = useState(false);
-  const [keyboardHeight] = useState(new Animated.Value(0));
-  const [buttonWidth] = useState(new Animated.Value(330));
   const navigation = useNavigation<SignupNavigationProp>();
-  const { userSocialType, socialAccessToken } = useUserStore();
-  const insets = useSafeAreaInsets();
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isValidFormat, setIsValidFormat] = useState(false);
-  const [isAvailable, setIsAvailable] = useState<null | boolean>(null);
-  const blurTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const [isTermsOpen, setIsTermsOpen] = useState(false);
-  const [checkedStates, setCheckedStates] = useState(Array(5).fill(false));
 
-  useEffect(() => {
-    const keyboardWillShow = Keyboard.addListener('keyboardWillShow', e => {
-      Animated.timing(keyboardHeight, {
-        toValue: e.endCoordinates.height,
-        duration: 450,
-        useNativeDriver: false,
-      }).start();
-    });
+  const { userSocialType, socialAccessToken } = useUserStore();
 
-    const keyboardWillHide = Keyboard.addListener('keyboardWillHide', () => {
-      Animated.timing(keyboardHeight, {
-        toValue: 0,
-        duration: 750,
-        useNativeDriver: false,
-      }).start();
-    });
+  const {
+    errorMessage,
+    isAvailable,
+    focus,
+    handleBlur,
+    handleClear,
+    handleNicknameChange,
+    setFocus,
+    userNickname,
+  } = useNicknameValidation();
 
-    return () => {
-      keyboardWillShow.remove();
-      keyboardWillHide.remove();
-    };
-  }, []);
+  const {
+    checkedStates,
+    setCheckedStates,
+    allChecked,
+    isRequiredAllChecked,
+    toggleAll,
+    toggleItem,
+  } = useTermsAgreement();
 
-  useEffect(() => {
-    Animated.timing(buttonWidth, {
-      toValue: 330,
-      duration: 250,
-      useNativeDriver: false,
-    }).start();
-  }, [focus]);
+  const { handleSignup } = useUserSignup({
+    checkedStates,
+    socialAccessToken,
+    userSocialType,
+    userNickname,
+    navigation,
+    setIsTermsOpen,
+  });
 
-  const handleSignup = async () => {
-    try {
-      const response = await signupApi({
-        socialAccessToken,
-        socialType: userSocialType,
-        userNickname,
-        userAgreementConsentRequestDto: {
-          ageConsent: checkedStates[0],
-          termsOfService: checkedStates[1],
-          privacyPolicy: checkedStates[2],
-          locationConsent: checkedStates[3],
-          marketingConsent: checkedStates[4],
-        },
-      });
-
-      console.log(checkedStates);
-      console.log(response);
-      setIsTermsOpen(false);
-
-      navigation.navigate('SelectBrand');
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleNicknameChange = (value: string) => {
-    if (blurTimeoutRef.current) clearTimeout(blurTimeoutRef.current);
-
-    setUserNickname(value);
-    setIsAvailable(null);
-    setErrorMessage('');
-
-    const error = validateFormat(value);
-    if (error) {
-      setErrorMessage(error);
-      setIsValidFormat(false);
-      return;
-    }
-
-    setIsValidFormat(true);
-  };
-
-  const handleBlur = () => {
-    setFocus(false);
-
-    if (!isValidFormat) return;
-
-    blurTimeoutRef.current = setTimeout(async () => {
-      try {
-        const res = await validateUserInfoApi(userNickname);
-        if (res.code === 14001) {
-          setIsAvailable(false);
-          setErrorMessage('이미 사용 중인 닉네임이에요');
-        } else {
-          setIsAvailable(true);
-          setErrorMessage('');
-        }
-      } catch (err) {
-        setIsAvailable(false);
-        setErrorMessage('서버 오류가 발생했어요');
-      }
-    }, 500);
-  };
-
-  const handleClear = () => {
-    setErrorMessage('');
-    setUserNickname('');
-    setIsAvailable(false);
-  };
+  const { keyboardHeight } = useButtonService({ focus });
 
   return (
-    <ScreenLayout className="h-full w-full flex-1 bg-white">
+    <ScreenLayout>
       <KeyboardAvoidingView
         behavior={Platform.select({ ios: 'padding', android: undefined })}
         className="flex-1">
-        <View className="mx-3 flex-1 p-2">
-          {/* Header h 수정 */}
-          <View className="h-12 flex-row items-center">
-            <Pressable onPress={() => navigation.goBack()} className="w-12">
-              <BackIcon />
-            </Pressable>
-            <View className="flex-1 items-center">
-              <Text className="text-center text-[20px] font-semibold text-black">
-                회원가입
-              </Text>
-            </View>
-            <View className="w-12" />
-          </View>
+        <SignupHeader text="회원가입" />
 
-          {/* Input Area */}
-          <View className="mt-10 items-start">
-            {/* <HighlightedText  text={TEXT} /> */}
-            <Text className="text-center text-[24px] font-semibold leading-9">
-              닉네임을 입력해주세요
-            </Text>
-            <Text className="mt-2 text-[16px] font-normal text-[#3B3E46]">
-              2-12자 이내로 작성해주세요
-            </Text>
-          </View>
-          <View>
-            <TextInput
-              className={cn(
-                userNickname.length > 0
-                  ? 'border-[#FF6C9A]'
-                  : 'border-[#B2B5BD]',
-                'mt-5 h-[56px] w-full rounded-2xl border pl-5 text-[16px] font-normal text-[#111114]',
-              )}
-              placeholder="닉네임을 입력해주세요"
-              placeholderTextColor="#B2B5BD"
-              maxLength={12}
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={handleNicknameChange}
-              onFocus={() => setFocus(true)}
-              onBlur={handleBlur}
-              value={userNickname}
-            />
-            {userNickname.length > 0 && (
-              <Pressable
-                className="absolute right-5 top-1/2 -translate-y-1/2"
-                onPress={handleClear}>
-                <CloseIcon />
-              </Pressable>
-            )}
-          </View>
+        <SignupIntro
+          title={'지금부터 당신은 [픽셀러]\n닉네임을 정해주세요'}
+          sub="2-12자 이내로 작성해주세요"
+          icon
+        />
 
-          {/* input 하단 텍스트 */}
-          <View className="mx-2 flex-row items-center justify-between">
-            <View className="mt-3 flex-row items-center">
-              {isAvailable === true ? (
-                <>
-                  <CheckIcon />
-                  <Text className="ml-3 text-[16px] font-normal text-[#000000]">
-                    사용할 수 있는 닉네임이에요
-                  </Text>
-                </>
-              ) : errorMessage ? (
-                <>
-                  <FailedIcon />
-                  <Text className="ml-3 text-[16px] font-normal text-[#FF6C6C]">
-                    {errorMessage}
-                  </Text>
-                </>
-              ) : null}
-            </View>
-            <View className="absolute right-0 top-3">
-              <Text className="text-[16px] font-normal text-[#B2B5BD]">
-                ({userNickname.length}/12)
-              </Text>
-            </View>
-          </View>
-        </View>
+        <NicknameInput
+          handleBlur={handleBlur}
+          handleChange={handleNicknameChange}
+          handleClear={handleClear}
+          setFocus={setFocus}
+          userNickname={userNickname}>
+          <NicknameFeedback
+            errorMessage={errorMessage}
+            isAvailable={isAvailable}
+            length={userNickname.length}
+          />
+        </NicknameInput>
 
-        {/* 버튼 (Animated 하단 위치) */}
-        <Animated.View
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            bottom: Animated.subtract(keyboardHeight, insets.bottom),
-            alignItems: 'center',
-          }}>
-          <Animated.View
-            style={{
-              width: buttonWidth,
-              height: 56,
-              backgroundColor: isAvailable || focus ? '#FF6C9A' : '#E5E6E9',
-              borderRadius: 40,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: focus ? 0 : 40,
-            }}>
-            <Pressable
-              onPress={
-                focus ? () => Keyboard.dismiss() : () => setIsTermsOpen(true)
-              }
-              disabled={focus ? false : !isAvailable}>
-              <Text className="text-[20px] font-bold text-white">
-                {focus ? '확인' : '다음'}
-              </Text>
-            </Pressable>
-          </Animated.View>
-        </Animated.View>
+        <NicknameSubmitButton
+          focus={focus}
+          isAvailable={isAvailable}
+          keyboardHeight={keyboardHeight}
+          setIsTermsOpen={setIsTermsOpen}
+        />
       </KeyboardAvoidingView>
 
       <TermsModal
@@ -266,7 +95,11 @@ const NicknameInputScreen = () => {
         handleSignup={handleSignup}
         visible={isTermsOpen}
         onClose={() => setIsTermsOpen(false)}
-        title="이용약관에 동의해주세요"
+        toggleAll={toggleAll}
+        allChecked={allChecked}
+        isRequiredAllChecked={isRequiredAllChecked}
+        toggleItem={toggleItem}
+        title="이용약관 동의"
       />
     </ScreenLayout>
   );

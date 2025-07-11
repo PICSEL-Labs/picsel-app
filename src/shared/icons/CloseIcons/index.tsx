db7b26a7c75313cc9cef458f1de729dd4ca58443
hyeone999 <caps026@naver.com>
2025-07-11 14:24:40 +0900

    ✨ Feat :  아이콘 컴포넌트 추가

--- src/shared/icons/CloseIcons/index.tsx
+++ src/shared/icons/CloseIcons/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import CloseGray from '@/assets/icons/close/icon-close-gray.svg';
+import CloseWhite from '@/assets/icons/close/icon-close-white.svg';
 import CloseBlack from '@/assets/icons/close/icon-close.svg';
 
 interface Props {
-  shape: 'black' | 'gray';
+  shape: 'black' | 'gray' | 'white';
   width: number;
   height: number;
 }
@@ -15,6 +16,8 @@ const CloseIcons = ({ shape, width, height }: Props) => {
       return <CloseBlack width={width} height={height} />;
     case 'gray':
       return <CloseGray width={width} height={height} />;
+    case 'white':
+      return <CloseWhite width={width} height={height} />;
     default:
       return <CloseBlack width={width} height={height} />;
   }
--- src/shared/icons/SearchIcons/index.tsx
+++ src/shared/icons/SearchIcons/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
+import SearchGray from '@/assets/icons/search/icon-search-gray.svg';
 import Search from '@/assets/icons/search/icon-search.svg';
 
 interface Props {
-  shape: 'default';
+  shape: 'black' | 'gray';
   width: number;
   height: number;
 }
 
 const SearchIcons = ({ shape, width, height }: Props) => {
   switch (shape) {
-    case 'default':
+    case 'black':
       return <Search width={width} height={height} />;
+    case 'gray':
+      return <SearchGray width={width} height={height} />;
     default:
       return <Search width={width} height={height} />;
   }

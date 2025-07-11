9c5813592716529abe39f8f16d5cf078d3f77274
hyeone999 <caps026@naver.com>
2025-07-11 14:23:36 +0900

    ✨ Feat :  shadow 스타일 공통 구현

--- src/styles/shadow.ts
+++ src/styles/shadow.ts
@@ -1,7 +1,7 @@
 export const commonShadow = {
   shadowColor: '#000',
   shadowOffset: { width: 2, height: 2 },
-  shadowOpacity: 0.3,
-  shadowRadius: 6,
+  shadowOpacity: 0.2,
+  shadowRadius: 2,
   elevation: 6,
 };

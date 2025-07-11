8c3c231d0797d4486cf6d27a2ce19d3e7ab3bd34
hyeone999 <caps026@naver.com>
2025-07-11 14:23:14 +0900

    ✨ Feat :  닉네임 input 폰트 스타일 수정

--- src/shared/constants/styles/nicknameInput.ts
+++ src/shared/constants/styles/nicknameInput.ts
@@ -1,2 +1,2 @@
 export const NICKNAME_INPUT =
-  'h-[56px] w-full rounded-2xl border px-5 font-nanum-square-regular text-[16px] text-primary-black';
+  'h-[56px] w-full rounded-2xl border px-5 font-nanum-square-ac-regular text-[16px] text-primary-black';

5479f3196683221b37946dcdad76dbb1673c38c5
hyeone999 <caps026@naver.com>
2025-07-12 21:13:28 +0900

    ✨ Feat :  브랜드 서비스 로직 구현

---
+++ src/feature/brand/model/hooks/useSelectedBrands.ts
@@ -0,0 +1,33 @@
+import { NavigationProp } from '@react-navigation/native';
+
+import { favoriteBrandApi } from '../../api/favoriteBrandApi';
+
+import { useSelectedBrandsStore } from '@/shared/store';
+
+export const useSelectedBrands = (navigation: NavigationProp<any>) => {
+  const { selectedList } = useSelectedBrandsStore();
+
+  const actualCount = selectedList.some(b => b.brandId === 'NONE')
+    ? 0
+    : selectedList.length;
+  const isDisabled = selectedList.length > 0;
+
+  const handleSelectedCompleted = async () => {
+    const brandIds = selectedList.map(value => value.brandId);
+
+    try {
+      const res = await favoriteBrandApi({ brandIds });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+
+    navigation.navigate('SignupSuccess');
+  };
+
+  return {
+    handleSelectedCompleted,
+    actualCount,
+    isDisabled,
+  };
+};

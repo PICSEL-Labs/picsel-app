bd11446d817cd1e264cafcecf088007d08e10447
hyeone999 <caps026@naver.com>
2025-07-12 22:36:42 +0900

    ✨ Feat :  가입 완료 페이지 구현

--- src/screens/signup/signupSuccess/index.tsx
+++ src/screens/signup/signupSuccess/index.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
-import { Pressable, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
+
+import SuccessHeader from '@/feature/success/ui/organisms/SuccessHeader';
+import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
+import { SignupNavigationProp } from '@/shared/types/navigateTypeUtil';
+import Button from '@/shared/ui/atoms/Button';
 
 const SignupSuccessScreen = () => {
-  return (
-    <SafeAreaView className="h-full w-full flex-1 bg-[#FF6C9A]">
-      <View className="flex-1 items-center justify-center gap-10">
-        <Text className="text-center text-[24px] font-semibold leading-[36px] text-[#FFFFFF]">
-          이제부터 당신은 픽셀러!{'\n'}세상의 모든 포토부스{'\n'}즐기러
-          가볼까요?
-        </Text>
-        <View className="h-[200px] w-[200px] bg-[#D9D9D9]" />
-      </View>
+  const navigation = useNavigation<SignupNavigationProp>();
 
-      <View className="items-center">
-        <Pressable className="mb-[20px] h-[56px] w-[330px] items-center justify-center rounded-[40px] bg-white">
-          <Text className="text-[20px] font-semibold text-[#FF6C9A]">
-            시작하기
-          </Text>
-        </Pressable>
+  return (
+    <ScreenLayout className="bg-primary-black">
+      <SuccessHeader />
+
+      <View className="mb-4 items-center">
+        <Button
+          text="시작하기"
+          color="white"
+          textColor="pink"
+          onPressIn={() => navigation.navigate('Home')}
+        />
       </View>
-    </SafeAreaView>
+    </ScreenLayout>
   );
 };
 

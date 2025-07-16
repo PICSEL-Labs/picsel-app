57e132b86f6a09735f625d6e69a95da0c6e21ee5
hyeone999 <caps026@naver.com>
2025-07-16 16:27:58 +0900

    💄 Design:  tooltip margin 값 수정

--- src/shared/ui/molecules/SocialLoginButton/index.tsx
+++ src/shared/ui/molecules/SocialLoginButton/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Pressable, View } from 'react-native';
 import { Text } from 'react-native';
-import { scale, verticalScale } from 'react-native-size-matters';
+import { scale } from 'react-native-size-matters';
 
 import { SocialTypes } from '@/feature/auth/login/types';
 import { TOOLTIP_CONFIG } from '@/shared/constants/tooltip';
@@ -31,17 +31,16 @@ const SocialLoginButton = ({ type, isLastLogin, onPressIn }: Props) => {
 
       {isLastLogin && (
         <View
-          className="absolute top-[72px] z-10 mt-1 items-center justify-center rounded-3xl bg-primary-pink"
+          className="j absolute top-[72px] z-10 mt-1 items-center rounded-3xl bg-primary-pink px-3 py-2"
           style={{
             width: scale(188),
-            height: verticalScale(37),
             ...boxShadow,
             ...config.offset,
           }}>
           <View className="absolute -top-1.5 z-0" style={config.tooltipDir}>
             <Tooltip shape={config.shape} />
           </View>
-          <Text className="text-center text-white body-rg-02">
+          <Text className="text-white body-rg-02">
             마지막으로 로그인한 계정이에요!
           </Text>
         </View>

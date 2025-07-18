06c6b7a444d8b0b0f88039c2cbe174da67813953
hyeone999 <caps026@naver.com>
2025-07-18 16:38:09 +0900

    ✨ Feat :  툴팁 인터랙션 추가

--- src/feature/auth/login/ui/organisms/LoginTooltip.tsx
+++ src/feature/auth/login/ui/organisms/LoginTooltip.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import { scale } from 'react-native-size-matters';
 
-import { SocialTypes } from '../../types';
-
 import { TOOLTIP_CONFIG } from '@/shared/constants/tooltip';
+import { useFloatingAnimation } from '@/shared/hooks/useFloatingAnimation';
 import Tooltip from '@/shared/icons/Tooltip';
+import { useUserStore } from '@/shared/store';
 import { boxShadow } from '@/styles/boxShadow';
 
-interface Props {
-  type: SocialTypes;
-}
+const LoginTooltip = () => {
+  const { userSocialType } = useUserStore();
 
-const LoginTooltip = ({ type }: Props) => {
-  const config = TOOLTIP_CONFIG[type];
+  const config = TOOLTIP_CONFIG[userSocialType] || TOOLTIP_CONFIG.GOOGLE;
+  const translateY = useFloatingAnimation({
+    toValue: 2,
+    duration1: 900,
+    duration2: 500,
+  });
+
+  if (!userSocialType) {
+    return null;
+  }
 
   return (
-    <View
-      className="j absolute top-[72px] z-10 mt-1 items-center rounded-3xl bg-primary-pink px-3 py-2"
+    <Animated.View
+      className="absolute top-[75px] z-10 items-center rounded-3xl bg-primary-pink px-3 py-2"
       style={{
         width: scale(188),
         ...boxShadow,
         ...config.offset,
+        transform: [{ translateY }],
       }}>
       <View className="absolute -top-1.5 z-0" style={config.tooltipDir}>
         <Tooltip shape={config.shape} />
@@ -30,7 +38,7 @@ const LoginTooltip = ({ type }: Props) => {
       <Text className="text-white body-rg-02">
         마지막으로 로그인한 계정이에요!
       </Text>
-    </View>
+    </Animated.View>
   );
 };
 

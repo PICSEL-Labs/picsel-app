38e673aa4421fc1cf128f18924e3a4d786892c82
hyeone999 <caps026@naver.com>
2025-07-22 15:52:13 +0900

    ✨ Feat :  사용자의 브랜드 리스트 전역적으로 storage 사용하여 저장

--- src/shared/store/brand/selectBrands/index.ts
+++ src/shared/store/brand/selectBrands/index.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface SelectedBrand {
   brandId: string;
@@ -11,36 +13,47 @@ interface BrandStore {
   removeBrand: (brandId: string) => void;
 }
 
-export const useSelectedBrandsStore = create<BrandStore>(set => ({
-  selectedList: [],
-  selectBrand: (brandId, name) =>
-    set(state => {
-      const isSelected = state.selectedList.some(
-        list => list.brandId === brandId,
-      );
+export const useSelectedBrandsStore = create<BrandStore>()(
+  persist(
+    set => ({
+      selectedList: [] as SelectedBrand[],
+      selectBrand: (brandId, name) =>
+        set(state => {
+          const isSelected = state.selectedList.some(
+            list => list.brandId === brandId,
+          );
 
-      if (brandId === 'NONE') {
-        return {
-          selectedList: isSelected ? [] : [{ brandId, name }],
-        };
-      }
+          if (brandId === 'NONE') {
+            return {
+              selectedList: isSelected ? [] : [{ brandId, name }],
+            };
+          }
 
-      const filtered = state.selectedList.filter(
-        list => list.brandId !== 'NONE',
-      );
+          const filtered = state.selectedList.filter(
+            list => list.brandId !== 'NONE',
+          );
 
-      if (isSelected) {
-        return {
-          selectedList: filtered.filter(list => list.brandId !== brandId),
-        };
-      } else {
-        return {
-          selectedList: [...filtered, { brandId, name }],
-        };
-      }
+          if (isSelected) {
+            return {
+              selectedList: filtered.filter(list => list.brandId !== brandId),
+            };
+          } else {
+            return {
+              selectedList: [...filtered, { brandId, name }],
+            };
+          }
+        }),
+
+      removeBrand: brandId =>
+        set(state => ({
+          selectedList: state.selectedList.filter(
+            list => list.brandId !== brandId,
+          ),
+        })),
     }),
-  removeBrand: brandId =>
-    set(state => ({
-      selectedList: state.selectedList.filter(list => list.brandId !== brandId),
-    })),
-}));
+    {
+      name: 'selected-brands-storage', // AsyncStorage key
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);

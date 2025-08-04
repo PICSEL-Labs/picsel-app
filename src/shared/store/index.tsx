f2b05cefb6cb7ab51a63e60612647359acd9f006
srchae <csulin77@gmail.com>
2025-08-04 23:57:15 +0900

    📦️ Chore :  useFilteredBrandsStore index 등록

--- src/shared/store/index.tsx
+++ src/shared/store/index.tsx
@@ -1,3 +1,4 @@
 export { useSelectedBrandsStore } from './brand/selectBrands';
 export { useBrandListStore } from './brand/brandList';
+export { useFilteredBrandsStore } from './brand/filterBrands';
 export { useUserStore } from './user';

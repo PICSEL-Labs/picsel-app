5bc207bbfe4fe30a9dcf96b9fb9f4f1d63654d8e
srchae <csulin77@gmail.com>
2025-08-04 23:38:05 +0900

    ✨ Feat :  초기화 아이콘 컴포넌트 수정

--- src/shared/icons/ReplayIcon/index.tsx
+++ src/shared/icons/ReplayIcon/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-import ReplayIcon from '@/assets/icons/replay/icon-replay.svg';
+import ReplayFalse from '@/assets/icons/replay/icon-replay-false.svg';
+import ReplayTrue from '@/assets/icons/replay/icon-replay-true.svg';
+import ReplayDefault from '@/assets/icons/replay/icon-replay.svg';
 
 interface Props {
-  shape: 'default';
+  shape: 'true' | 'false';
   width: number;
   height: number;
 }
 
 const ReplayIcons = ({ shape, width, height }: Props) => {
   switch (shape) {
-    case 'default':
-      return <ReplayIcon width={width} height={height} />;
+    case 'true':
+      return <ReplayTrue width={width} height={height} />;
+    case 'false':
+      return <ReplayFalse width={width} height={height} />;
     default:
-      return <ReplayIcon width={width} height={height} />;
+      return <ReplayDefault width={width} height={height} />;
   }
 };
 

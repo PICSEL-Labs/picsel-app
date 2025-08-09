0117d19f692cf48b41bd31f436bb4938d46d36a6
hyeone999 <caps026@naver.com>
2025-08-09 14:00:54 +0900

    ✨ Feat :  map 검색에 필요한 설정값 상수로 구현

---
+++ src/feature/map/constants/mapConfig.ts
@@ -0,0 +1,11 @@
+export const MAP_SEARCH_CONFIG = {
+  MAX_OFFSET: 0.02,
+  MIN_ZOOM_LEVEL: 15.5,
+  DEFAULT_PAGE_SIZE: 200,
+  ZOOM_OFFSETS: {
+    18: 0.005,
+    16: 0.015,
+    14: 0.03,
+    DEFAULT: 0.05,
+  },
+} as const;

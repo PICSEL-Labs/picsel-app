4784652a48e41a13db938b5e31dc91f96e34e9d0
hyeone999 <caps026@naver.com>
2025-08-09 15:47:21 +0900

    ✨ Feat :  selected props 적용

--- src/feature/map/ui/organisms/DefaultMarker.tsx
+++ src/feature/map/ui/organisms/DefaultMarker.tsx
@@ -3,18 +3,29 @@ import React from 'react';
 import { Image } from 'react-native';
 import Config from 'react-native-config';
 
+import SelectedStoreMarker from '../../components/SelectedStoreMarker';
+
 interface Props {
   brandIconImageUrl: string;
+  selected: boolean;
 }
 
-const DefaultMarker = ({ brandIconImageUrl }: Props) => {
-  return (
+const DefaultMarker = ({ brandIconImageUrl, selected }: Props) => {
+  return selected ? (
+    <SelectedStoreMarker brandIconImageUrl={brandIconImageUrl} />
+  ) : (
     <Image
       source={{ uri: Config.IMAGE_URL + brandIconImageUrl }}
       width={28}
       height={28}
       className="rounded-full border-2 border-white"
-      style={{ boxShadow: '0px 2px 6px 0px rgba(0, 0, 0, 0.25)' }}
+      // 임시 shadow value 추후 쉐도우 스타일 코드 구현 후 수정
+      style={{
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+      }}
     />
   );
 };

1433bb3c2271791f3ca142ce6e931c82fa5993c0
hyeone999 <caps026@naver.com>
2025-08-09 14:02:41 +0900

    ✨ Feat :  지도 검색 관련 타입 interface로 정리

---
+++ src/feature/map/types/index.ts
@@ -0,0 +1,42 @@
+export interface StoreSearchParams {
+  minX: number;
+  maxX: number;
+  minY: number;
+  maxY: number;
+  page: number;
+  size: number;
+  centerX: number;
+  centerY: number;
+}
+
+// 단일 매장 정보
+export interface StoreData {
+  brandId: string;
+  brandName: string;
+  brandIconImageUrl: string;
+  storeId: string;
+  storeName: string;
+  address: string;
+  x: number; // 경도
+  y: number; // 위도
+  isFavorite: boolean;
+}
+
+// 실제 content 포함한 내부 응답 구조
+export interface StoreSearchResult {
+  content: StoreData[];
+  totalElements: number;
+  totalPages: number;
+  pageNumber: number;
+  pageSize: number;
+  first: boolean;
+  last: boolean;
+}
+
+// 서버 전체 응답 구조
+export interface StoreSearchResponse {
+  code: number;
+  codeMessage: string;
+  message: string;
+  data: StoreSearchResult;
+}

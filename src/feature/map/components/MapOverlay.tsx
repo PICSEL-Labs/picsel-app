2576422ed1cc11c36453afe2ccea4f354a34e06c
hyeone999 <caps026@naver.com>
2025-08-12 15:38:39 +0900

    ✨ Feat :  마커 이미지 렌더링 이슈로 인한 이미지 프리렌더 구현

--- src/feature/map/components/MapOverlay.tsx
+++ src/feature/map/components/MapOverlay.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { NaverMapMarkerOverlay } from '@mj-studio/react-native-naver-map';
+import { Image } from 'react-native';
+import Config from 'react-native-config';
 
+import { useMarker } from '../hooks/useMarker';
 import { Store } from '../types';
 import DefaultMarker from '../ui/organisms/DefaultMarker';
 
@@ -9,19 +12,36 @@ interface Props {
   stores?: Store[];
 }
 
-export const MapOverlay = ({ stores }: Props) => {
-  const [selectedMarkerId, setSelectedMarkerId] = useState<string | null>(null);
+const MapOverlay = ({ stores }: Props) => {
+  const [shouldRenderMarkers, setShouldRenderMarkers] = useState(false);
+  const { handleMarkerPress, selectedMarkerId } = useMarker();
 
-  const handleMarkerPress = (storeId: string) => {
-    setSelectedMarkerId(prev => (prev === storeId ? null : storeId));
-  };
+  useEffect(() => {
+    if (stores?.length) {
+      setShouldRenderMarkers(false);
+
+      const preloadPromises = stores.map(store =>
+        Image.prefetch(Config.IMAGE_URL + store.brandIconImageUrl),
+      );
+
+      Promise.all(preloadPromises)
+        .then(() => {
+          setTimeout(() => setShouldRenderMarkers(true), 100);
+        })
+        .catch(() => {
+          setTimeout(() => setShouldRenderMarkers(true), 500);
+        });
+    }
+  }, [stores]);
+
+  if (!shouldRenderMarkers) {
+    return null;
+  }
 
   return (
     <>
       {stores?.map(store => {
         const isSelected = selectedMarkerId === store.storeId;
-
-        console.log(isSelected);
 
         return (
           <NaverMapMarkerOverlay
@@ -39,3 +59,5 @@ export const MapOverlay = ({ stores }: Props) => {
     </>
   );
 };
+
+export default MapOverlay;

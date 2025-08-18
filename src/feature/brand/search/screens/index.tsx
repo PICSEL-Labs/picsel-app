1ce73d054245f3a0f8c981eb6a5ad19f38fba511
srchae <csulin77@gmail.com>
2025-08-18 23:55:08 +0900

    ✨ Feat :  브랜드 검색 스크린 구현

---
+++ src/feature/brand/search/screens/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+
+import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
+import SparkleImages from '@/shared/images/Sparkle';
+import { RootStackNavigationProp } from '@/shared/types/navigateTypeUtil';
+import Input from '@/shared/ui/atoms/Input';
+
+const BrandSearchScreen = () => {
+  const navigation = useNavigation<RootStackNavigationProp>();
+  const [query, setQuery] = useState('');
+
+  const handleClear = () => {
+    setQuery('');
+  };
+
+  const handleBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+      accessible={false}>
+      <ScreenLayout>
+        <Input
+          value={query}
+          onChangeText={setQuery}
+          handleClear={handleClear}
+          arrow
+          onPressLeft={handleBack}
+          close
+          autoFocus
+        />
+
+        <View className="w-full flex-1 items-center justify-center">
+          <SparkleImages shape="bg-opacity" height={418} width={340} />
+        </View>
+      </ScreenLayout>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default BrandSearchScreen;

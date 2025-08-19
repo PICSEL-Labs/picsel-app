c19626baa69c23421463a7f62d39287a69c12737
hyeone999 <caps026@naver.com>
2025-08-19 16:31:04 +0900

    ✨ Feat :  메모리 최적화를 위해 렌더링 로직 memo hooks 적용

--- src/feature/map/ui/organisms/MapOverlay.tsx
+++ src/feature/map/ui/organisms/MapOverlay.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { NaverMapMarkerOverlay } from '@mj-studio/react-native-naver-map';
 import Config from 'react-native-config';
 
 import { Store } from '../../types';
-import DefaultMarker from '../atoms/DefaultMarker';
-import SelectedStoreMarker from '../atoms/SelectedStoreMarker';
+import StoreMarker from '../atoms/StoreMarker';
 
 interface Props {
   stores?: Store[];
@@ -14,29 +13,28 @@ interface Props {
 }
 
 const MapOverlay = ({ stores, selectedMarkerId, handleMarkerPress }: Props) => {
-  return (
-    <>
-      {stores?.map(store => {
-        const imageSource = {
-          uri: Config.IMAGE_URL + store.brandIconImageUrl,
-        };
+  const markers = useMemo(() => {
+    return stores?.map(store => {
+      const imageSource = {
+        uri: `${Config.IMAGE_URL}${store.brandIconImageUrl}?w=50&h=50`,
+      };
 
-        return (
-          <NaverMapMarkerOverlay
-            key={store.storeId}
-            latitude={store.y}
-            longitude={store.x}
-            onTap={() => handleMarkerPress(store.storeId)}>
-            {selectedMarkerId === store.storeId ? (
-              <SelectedStoreMarker imageSource={imageSource} />
-            ) : (
-              <DefaultMarker imageSource={imageSource} />
-            )}
-          </NaverMapMarkerOverlay>
-        );
-      })}
-    </>
-  );
+      return (
+        <NaverMapMarkerOverlay
+          key={`${store.storeId}-${selectedMarkerId === store.storeId}`}
+          latitude={store.y}
+          longitude={store.x}
+          onTap={() => handleMarkerPress(store.storeId)}>
+          <StoreMarker
+            imageSource={imageSource}
+            isSelected={selectedMarkerId === store.storeId}
+          />
+        </NaverMapMarkerOverlay>
+      );
+    });
+  }, [stores, selectedMarkerId]);
+
+  return <>{markers}</>;
 };
 
 export default MapOverlay;

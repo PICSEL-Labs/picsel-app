268f7ff15f2f6b7d4fed3acc87a1cb5592c90dfc
srchae <csulin77@gmail.com>
2025-09-03 23:10:08 +0900

    ✨ Feat :  검색 화면 재생성 및 검색 결과 리스트 컴포넌트 적용

---
+++ src/screens/search/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+
+import NoResult from '@/feature/brand/ui/organisms/NoResult';
+import { useStoreSearch } from '@/feature/search/hooks/useStoreSearch';
+import SearchResultList from '@/feature/search/ui/organisms/SearchResultList';
+import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
+import { RootStackNavigationProp } from '@/shared/types/navigateTypeUtil';
+import Input from '@/shared/ui/atoms/Input';
+
+const StoreSearchScreen = () => {
+  const navigation = useNavigation<RootStackNavigationProp>();
+  const [query, setQuery] = useState('');
+
+  const { result, uiState, hasResults } = useStoreSearch(query);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+      accessible={false}>
+      <ScreenLayout>
+        <Input
+          value={query}
+          onChangeText={setQuery}
+          handleClear={() => setQuery('')}
+          arrow
+          onPressLeft={() => navigation.goBack()}
+          close
+          autoFocus
+        />
+        <View className="flex-1">
+          <SearchResultList
+            data={result}
+            highlight={query}
+            onPressItem={row => {
+              console.log(row);
+            }}
+          />
+          {!hasResults && <NoResult visible={uiState === 'noResults'} />}
+        </View>
+      </ScreenLayout>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default StoreSearchScreen;

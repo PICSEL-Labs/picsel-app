966923fa1e518a5b08cb6ac33a06e8608006a894
hyeone999 <caps026@naver.com>
2025-09-10 17:12:07 +0900

    🎨 Style :  기본 버튼 shadow 스타일 코드 추가

--- src/styles/shadows/index.ts
+++ src/styles/shadows/index.ts
@@ -14,6 +14,14 @@ export const inputShadow = {
   elevation: 4,
 };
 
+export const defaultButtonShadow = {
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.2,
+  shadowRadius: 4,
+  elevation: 4,
+};
+
 export const insetShadow = {
   default:
     '-2px -4px 12px 0px rgba(255, 255, 255, 0.10) inset, -2px -2px 8px 0px rgba(0, 0, 0, 0.20) inset, 2px 4px 8px 0px rgba(255, 255, 255, 0.25) inset',

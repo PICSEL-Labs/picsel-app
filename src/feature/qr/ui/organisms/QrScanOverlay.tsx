3f17fcd91d440846628ac8af53fefbfef7c62180
srchae <csulin77@gmail.com>
2025-09-30 23:41:44 +0900

    ♻️ Refactor :  상수값 camelCase 네이밍에서 UPPER_SNAKE_CASE로 개선

--- src/feature/qr/ui/organisms/QrScanOverlay.tsx
+++ src/feature/qr/ui/organisms/QrScanOverlay.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import { Dimensions, View } from 'react-native';
 
+import QrScanHeader from '../layout/QrScanHeader';
 import { QrScanBackground } from '../molecules/QrScanBackground';
 import QrScanContent from '../molecules/QrScanContent';
 import QrScanFooter from '../molecules/QrScanFooter';
 import { QrScanFrame } from '../molecules/QrScanFrame';
-import QrScanHeader from '../molecules/QrScanHeader';
 
 const { width, height } = Dimensions.get('window');
 const BOX_SIZE = width * 0.7;
-const frameTop = (height - BOX_SIZE) / 2;
-const frameLeft = (width - BOX_SIZE) / 2;
+const FRAME_TOP = (height - BOX_SIZE) / 2;
+const FRAME_LEFT = (width - BOX_SIZE) / 2;
 
 const QrScanOverlay = () => {
   return (
@@ -20,8 +20,8 @@ const QrScanOverlay = () => {
         width={width}
         height={height}
         boxSize={BOX_SIZE}
-        frameTop={frameTop}
-        frameLeft={frameLeft}
+        frameTop={FRAME_TOP}
+        frameLeft={FRAME_LEFT}
       />
 
       <View className="z-20 flex-1 items-center justify-between">
@@ -29,14 +29,14 @@ const QrScanOverlay = () => {
         <View
           className="absolute items-center"
           style={{
-            top: frameTop - 80, // 프레임 바로 위에 텍스트 배치
+            top: FRAME_TOP - 80, // 프레임 바로 위에 텍스트 배치
           }}>
           <QrScanContent />
         </View>
         <QrScanFrame
           boxSize={BOX_SIZE}
-          frameTop={frameTop}
-          frameLeft={frameLeft}
+          frameTop={FRAME_TOP}
+          frameLeft={FRAME_LEFT}
         />
         <QrScanFooter />
       </View>

48c68257c656ec42549a4a896a5f3bf891603943
hyeone999 <caps026@naver.com>
2025-10-15 12:33:41 +0900

    ✨ Feat :  기존 qr icon component에 white qr icon 추가하여 구현

--- src/shared/icons/QrIcons/index.tsx
+++ src/shared/icons/QrIcons/index.tsx
@@ -5,9 +5,10 @@ import QrOff1 from '@/assets/icons/qr/icon-qr-primary-off-1.svg';
 import QrOff2 from '@/assets/icons/qr/icon-qr-primary-off-2.svg';
 import QrOn1 from '@/assets/icons/qr/icon-qr-primary-on-1.svg';
 import QrOn2 from '@/assets/icons/qr/icon-qr-primary-on-2.svg';
+import QrWhite from '@/assets/icons/qr/icon-qr-white.svg';
 
 interface Props {
-  shape: 'off-1' | 'off-2' | 'on-1' | 'on-2' | 'pink';
+  shape: 'off-1' | 'off-2' | 'on-1' | 'on-2' | 'pink' | 'white';
   width: number;
   height: number;
 }
@@ -24,6 +25,8 @@ const QrIcons = ({ shape, width, height }: Props) => {
       return <QrOn2 width={width} height={height} />;
     case 'pink':
       return <QrPink width={width} height={height} />;
+    case 'white':
+      return <QrWhite width={width} height={height} />;
     default:
       return <QrOff1 width={width} height={height} />;
   }

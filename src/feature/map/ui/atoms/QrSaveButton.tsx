5d9b514e444473df7cc6f2c42c67d037eb31e15a
hyeone999 <caps026@naver.com>
2025-10-15 12:33:16 +0900

    ✨ Feat :  cta button pressed에 따라 스타일 분기처리 구현

--- src/feature/map/ui/atoms/QrSaveButton.tsx
+++ src/feature/map/ui/atoms/QrSaveButton.tsx
@@ -22,8 +22,7 @@ const QrSaveButton = () => {
       className={`flex h-[56px] w-[343px] shrink-0 flex-row items-center justify-center space-x-2 rounded-[40px] px-6 py-4 text-center ${
         isPressed ? 'bg-pink-500' : 'bg-white'
       }`}>
-      {/* qr white icon 필요 -> 디자인 파트 문의 */}
-      <QrIcons shape={isPressed ? 'off-1' : 'on-1'} width={24} height={24} />
+      <QrIcons shape={isPressed ? 'white' : 'on-1'} width={24} height={24} />
       <Text
         className={`headline-02 ${isPressed ? 'text-white' : 'text-pink-600'}`}>
         QR 사진 저장

bd84d2322e0f2801673718e47a909e082b2bbb40
hyeone999 <caps026@naver.com>
2025-10-15 17:42:42 +0900

    ✨ Feat :  이 근처 브랜드 bottomSheet 및 매장 상세 bottomSheet 홈화면에 추가하여 구현

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import {
   NaverMapView,
@@ -8,11 +8,15 @@ import { useNavigation } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
 
 import BrandFilterBottomSheet from '@/feature/brand/ui/organisms/BrandFilterBottomSheet';
-import { useLocationPermission } from '@/feature/map/hooks/useLocationPermission'; // ✅ 추가
+import { useBottomSheetManager } from '@/feature/map/hooks/useBottomSheetManager';
+import { useLocationPermission } from '@/feature/map/hooks/useLocationPermission';
+import { useMapActions } from '@/feature/map/hooks/useMapActions';
 import { useMapCamera } from '@/feature/map/hooks/useMapCamera';
+import { useMapEffects } from '@/feature/map/hooks/useMapEffects';
 import { useMapSearch } from '@/feature/map/hooks/useMapSearch';
 import { useMarker } from '@/feature/map/hooks/useMarker';
 import { useFetchStores } from '@/feature/map/queries/useFetchStores';
+import BrandDetailBottomSheet from '@/feature/map/ui/organisms/BrandDetailBottomSheet';
 import MapActionButton from '@/feature/map/ui/organisms/MapActionButton';
 import MapOverlay from '@/feature/map/ui/organisms/MapOverlay';
 import NearbyBrandBottomSheet from '@/feature/map/ui/organisms/NearbyBrandBottomSheet';
@@ -38,29 +42,32 @@ const HomeScreen = () => {
     useMapCamera();
   const { setSelectedMarkerId, selectedMarkerId, handleMarkerPress } =
     useMarker();
+  const {
+    nearbyBrandVisible,
+    detailBrandVisible,
+    hideAllSheet,
+    hideSheet,
+    showSheet,
+  } = useBottomSheetManager();
 
-  const [nearbyBrandVisible, setNearbyBrandVisible] = useState(true);
+  useMapEffects({
+    getCurrentLocation,
+    mapRef,
+    selectedMarkerId,
+    hideSheet,
+    showSheet,
+  });
 
-  // 앱 최초 실행 시 현재 위치로 이동(위치 권한이 있는 경우)
-  useEffect(() => {
-    (async () => {
-      const location = await getCurrentLocation();
-      if (location) {
-        mapRef.current?.animateCameraTo({
-          ...location,
-          zoom: 15,
-          duration: 500,
-        });
-      }
-    })();
-  }, [getCurrentLocation]);
-
-  const handleLocationSearch = () => {
-    searchStoresByLocation(camera.latitude, camera.longitude, camera.zoom);
-    setSelectedMarkerId(null);
-    hideSearchButton();
-    setActiveButton('brand');
-  };
+  const { handleLocationSearch, handleNavigateSearch } = useMapActions({
+    searchStoresByLocation,
+    setSelectedMarkerId,
+    hideSearchButton,
+    setActiveButton,
+    showSheet,
+    hideAllSheet,
+    navigation,
+    camera,
+  });
 
   return (
     <ScreenLayout>
@@ -91,7 +98,7 @@ const HomeScreen = () => {
         value={brandName}
         onChangeText={setBrandName}
         handleClear={() => setBrandName('')}
-        onPress={() => navigation.navigate('StoreSearch')}
+        onPress={handleNavigateSearch}
         placeholder="브랜드명, 매장명, 위치 검색"
         search
         close
@@ -107,13 +114,19 @@ const HomeScreen = () => {
         closeModal={closeModal}
       />
 
+      <BrandFilterBottomSheet visible={isModalOpen} onClose={closeModal} />
+
       <NearbyBrandBottomSheet
         visible={nearbyBrandVisible}
         brands={stores?.data.brands}
-        onClose={() => setNearbyBrandVisible(false)}
+        showSheet={() => showSheet('nearby')}
+        hideSheet={() => hideSheet('nearby')}
       />
 
-      <BrandFilterBottomSheet visible={isModalOpen} onClose={closeModal} />
+      <BrandDetailBottomSheet
+        visible={detailBrandVisible}
+        onClose={() => hideSheet('detail')}
+      />
     </ScreenLayout>
   );
 };

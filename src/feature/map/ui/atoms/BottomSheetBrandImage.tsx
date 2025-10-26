5486eae30e961d899eb755d6b29a5224b9b2c7e8
hyeone999 <caps026@naver.com>
2025-10-26 12:15:17 +0900

    ✨ Feat :  브랜드 선호 mutation 적용

--- src/feature/map/ui/atoms/BottomSheetBrandImage.tsx
+++ src/feature/map/ui/atoms/BottomSheetBrandImage.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { View, Image, Pressable } from 'react-native';
 import Config from 'react-native-config';
 
+import { useBottomSheetManager } from '../../hooks/useBottomSheetManager';
+import { useToggleFavoriteBrand } from '../../mutations/useToggleFavoriteBrand';
 import { MapBottomSheetProps } from '../../types';
 
 import BrandFavIcons from '@/shared/icons/BrandFav';
@@ -11,29 +13,36 @@ import { defaultShadow } from '@/styles/shadows';
 const BottomSheetBrandImage = ({
   imageUrl,
   nearBy = false,
-  isFavorite,
+  isFavorite = false,
   brandId,
 }: MapBottomSheetProps) => {
+  const { isPending } = useToggleFavoriteBrand();
+  const { handleToggleFavorite } = useBottomSheetManager({
+    brandId,
+    isFavorite,
+    isPending,
+  });
+
   return (
-    <View
-      id={brandId}
-      style={defaultShadow}
-      className={nearBy ? 'items-center' : undefined}>
+    <View style={defaultShadow} className={nearBy ? 'items-center' : undefined}>
       <Image
         className="h-[60px] w-[60px] rounded-full"
         source={{ uri: Config.IMAGE_URL + imageUrl }}
         resizeMode="cover"
       />
 
-      <Pressable
-        className="absolute bottom-0 right-0"
-        onPress={() => console.log(brandId)}>
-        <BrandFavIcons
-          width={23}
-          height={23}
-          shape={isFavorite ? 'empty' : 'gray'}
-        />
-      </Pressable>
+      {brandId && (
+        <Pressable
+          className="absolute bottom-0 right-0"
+          onPress={handleToggleFavorite}
+          disabled={isPending}>
+          <BrandFavIcons
+            width={23}
+            height={23}
+            shape={isFavorite ? 'fill' : 'gray'}
+          />
+        </Pressable>
+      )}
     </View>
   );
 };

9e04ebc29b69d46c2f807b3ed791e1baab0d473b
hyeone999 <caps026@naver.com>
2025-11-19 10:01:17 +0900

    ✨ Feat : 검색화면애서 params로 위치값을 홈으로 전달

--- src/screens/search/index.tsx
+++ src/screens/search/index.tsx
@@ -7,14 +7,44 @@ import NoResult from '@/feature/brand/ui/organisms/NoResult';
 import { useStoreSearch } from '@/feature/search/hooks/useStoreSearch';
 import SearchResultList from '@/feature/search/ui/organisms/SearchResultList';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
+import { useMapLocationStore } from '@/shared/store';
 import { RootStackNavigationProp } from '@/shared/types/navigateTypeUtil';
 import Input from '@/shared/ui/atoms/Input';
 
 const StoreSearchScreen = () => {
   const navigation = useNavigation<RootStackNavigationProp>();
   const [query, setQuery] = useState('');
+  const { setTargetLocation } = useMapLocationStore();
 
   const { result, uiState, hasResults } = useStoreSearch(query);
+
+  const handleItemPress = (row: {
+    id: string;
+    kind: string;
+    title: string;
+    subtitle: string;
+    distanceMeters: number;
+    x: number;
+    y: number;
+  }) => {
+    // 전역 상태에 저장
+    setTargetLocation(
+      {
+        latitude: row.y,
+        longitude: row.x,
+        zoom: 15,
+      },
+      {
+        id: row.id,
+        kind: row.kind,
+        title: row.title,
+        subtitle: row.subtitle,
+      },
+    );
+
+    // Home으로 이동
+    navigation.navigate('Home');
+  };
 
   return (
     <TouchableWithoutFeedback
@@ -36,9 +66,7 @@ const StoreSearchScreen = () => {
           <SearchResultList
             data={result}
             highlight={query.split(/\s+/)}
-            onPressItem={row => {
-              console.log(row);
-            }}
+            onPressItem={handleItemPress}
           />
           {!hasResults && <NoResult visible={uiState === 'noResults'} />}
         </View>

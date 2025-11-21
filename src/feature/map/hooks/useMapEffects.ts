af6590f65e515f4faacc12f007895f4a72f67df6
hyeone999 <caps026@naver.com>
2025-11-21 13:27:27 +0900

    ✨ Feat : 카메라 이동시에도 계속해서 유저의 위치로 카메라가 옮겨지는 비즈니스 로직 제거

--- src/feature/map/hooks/useMapEffects.ts
+++ src/feature/map/hooks/useMapEffects.ts
@@ -1,38 +1,16 @@
 import { useEffect } from 'react';
 
-import { NaverMapViewRef } from '@mj-studio/react-native-naver-map';
-
 interface UseMapEffectsParams {
-  getCurrentLocation: () => Promise<{
-    latitude: number;
-    longitude: number;
-  } | null>;
-  mapRef: React.RefObject<NaverMapViewRef>;
   selectedMarkerId: string | null;
   showSheet: (type: 'nearby' | 'detail') => void;
   hideSheet: (type: 'nearby' | 'detail') => void;
 }
 
 export const useMapEffects = ({
-  getCurrentLocation,
-  mapRef,
   selectedMarkerId,
   showSheet,
   hideSheet,
 }: UseMapEffectsParams) => {
-  useEffect(() => {
-    (async () => {
-      const location = await getCurrentLocation();
-      if (location) {
-        mapRef.current?.animateCameraTo({
-          ...location,
-          zoom: 15,
-          duration: 500,
-        });
-      }
-    })();
-  }, [getCurrentLocation, mapRef]);
-
   useEffect(() => {
     if (selectedMarkerId) {
       showSheet('detail');

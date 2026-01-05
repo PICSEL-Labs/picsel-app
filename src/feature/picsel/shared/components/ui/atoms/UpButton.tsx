12ee0ce44987fd2a23c8c25a28aeb43414d9289d
hyeone999 <caps026@naver.com>
2026-01-05 09:27:02 +0900

    ✨ Feat : 상단 스크롤 플로팅 버튼 구현

---
+++ src/feature/picsel/shared/components/ui/atoms/UpButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Pressable, View } from 'react-native';
+
+import FloatingButton from '@/shared/icons/FloatingButton';
+
+interface Props {
+  onPress: () => void;
+}
+
+const UpButton = ({ onPress }: Props) => {
+  return (
+    <View className="absolute bottom-14 left-[4.7px]">
+      <Pressable onPress={onPress}>
+        <FloatingButton shape="floating" />
+      </Pressable>
+    </View>
+  );
+};
+
+export default UpButton;

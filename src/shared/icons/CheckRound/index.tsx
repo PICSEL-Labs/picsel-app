577825b334967bd806ee1dbe732af19b14971b50
hyeone999 <caps026@naver.com>
2026-01-11 13:36:36 +0900

    ✨ Feat : 아이콘 컴포넌트 에셋 추가 및 아이콘 구현

--- src/shared/icons/CheckRound/index.tsx
+++ src/shared/icons/CheckRound/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import CheckDefault from '@/assets/icons/checkRound/icon-check-default.svg';
+import CheckRoundPink from '@/assets/icons/checkRound/icon-check-pink.svg';
 import CheckRound from '@/assets/icons/checkRound/icon-check-round.svg';
 
 interface Props {
-  shape: 'check-round' | 'default';
+  shape: 'check-round' | 'default' | 'pink';
   width: number;
   height: number;
 }
@@ -13,6 +14,8 @@ const CheckRoundIcons = ({ shape, width, height }: Props) => {
   switch (shape) {
     case 'check-round':
       return <CheckRound width={width} height={height} />;
+    case 'pink':
+      return <CheckRoundPink width={width} height={height} />;
     case 'default':
       return <CheckDefault width={width} height={height} />;
     default:

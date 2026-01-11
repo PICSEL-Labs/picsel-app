f1fd2d49adc048f3b90c4fad37481b1c752dce7b
hyeone999 <caps026@naver.com>
2026-01-11 13:54:55 +0900

    ✨ Feat : 픽셀북 bottomSheet 구현

--- src/feature/picsel/shared/components/ui/template/picselBook/PicselBookTemplate.tsx
+++ src/feature/picsel/shared/components/ui/template/picselBook/PicselBookTemplate.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { View } from 'react-native';
 
 import EmptyStateLayout from '../../layouts/EmptyStateLayout';
 import EmptyMessage from '../../molecules/EmptyMessage';
+import PicselBookBottomSheet from '../../organisms/bottomSheet/PicselBookBottomSheet';
 
 import AddButton from '@/feature/picsel/shared/components/ui/atoms/AddButton';
 import AddBookButton from '@/feature/picsel/shared/components/ui/organisms/AddBookButton';
 import { usePicselBookActions } from '@/feature/picsel/shared/hooks/usePicselBookActions';
 
 const PicselBookTemplate = () => {
-  const { handleAddPicsel, handleAddBook } = usePicselBookActions();
+  const { handleAddPicsel } = usePicselBookActions();
+  const picselBookRef = useRef<BottomSheetModal>(null);
+
+  const handleAddBook = () => {
+    picselBookRef.current?.present();
+  };
+
+  const handleSubmit = (bookName: string) => {
+    // TODO: 픽셀북 생성 API 호출
+    console.log('픽셀북 생성:', bookName);
+  };
 
   return (
     <EmptyStateLayout floatingButton={<AddButton onPress={handleAddPicsel} />}>
       <View className="flex-1">
         <AddBookButton onPress={handleAddBook} />
+
         <EmptyMessage
           message="네컷사진을 모아두는 나만의 앨범,픽셀북을 추가해보세요!"
           breakAtComma
         />
       </View>
+
+      <PicselBookBottomSheet ref={picselBookRef} onSubmit={handleSubmit} />
     </EmptyStateLayout>
   );
 };

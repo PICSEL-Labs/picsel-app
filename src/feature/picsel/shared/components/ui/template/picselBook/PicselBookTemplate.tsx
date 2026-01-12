3cea3c54ed2b8ecdbb639af96fc582b6145c9f73
hyeone999 <caps026@naver.com>
2026-01-12 11:54:27 +0900

    ✨ Feat : 픽셀북 템플릿 구현

--- src/feature/picsel/shared/components/ui/template/picselBook/PicselBookTemplate.tsx
+++ src/feature/picsel/shared/components/ui/template/picselBook/PicselBookTemplate.tsx
@@ -6,10 +6,12 @@ import { View } from 'react-native';
 import EmptyStateLayout from '../../layouts/EmptyStateLayout';
 import EmptyMessage from '../../molecules/EmptyMessage';
 import PicselBookBottomSheet from '../../organisms/bottomSheet/PicselBookBottomSheet';
+import PicselBookList from '../../organisms/PicselBookList';
 import PixelToolbar from '../../organisms/PixelToolbar';
 
 import AddButton from '@/feature/picsel/shared/components/ui/atoms/AddButton';
 import AddBookButton from '@/feature/picsel/shared/components/ui/organisms/AddBookButton';
+import { MOCK_PICSEL_BOOK_DATA } from '@/feature/picsel/shared/data/mockPicselBookData';
 import { usePhotoSelection } from '@/feature/picsel/shared/hooks/usePhotoSelection';
 import { usePicselBookActions } from '@/feature/picsel/shared/hooks/usePicselBookActions';
 import {
@@ -17,7 +19,6 @@ import {
   PICSEL_BOOK_SORT_OPTIONS,
   useSortActionSheet,
 } from '@/feature/picsel/shared/hooks/useSortActionSheet';
-import PicselBookIcons from '@/shared/icons/PicselBookIcons';
 import { useToastStore } from '@/shared/store/ui/toast';
 
 const PicselBookTemplate = () => {
@@ -25,9 +26,10 @@ const PicselBookTemplate = () => {
   const { showToast } = useToastStore();
   const picselBookRef = useRef<BottomSheetModal>(null);
 
-  // TODO: 실제 픽셀북 데이터로 교체
-  const [photoData] = useState([]);
-  const totalPhotos = photoData.length;
+  // 픽셀북 데이터
+  const [books, setBooks] = useState(MOCK_PICSEL_BOOK_DATA);
+  const totalBooks = books.length;
+  const hasBooks = totalBooks > 0;
 
   // 선택 관련 훅
   const {
@@ -58,37 +60,74 @@ const PicselBookTemplate = () => {
     // TODO: 픽셀북 생성 API 호출
     console.log('픽셀북 생성:', bookName);
     showToast(`"${bookName}" 픽셀북이 생성되었어요`, 60);
+
+    // 임시로 새 픽셀북 추가
+    const newBook = {
+      id: String(books.length + 1),
+      title: bookName,
+      photoCount: 0,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+    setBooks([newBook, ...books]);
   };
 
+  const handleBookPress = (bookId: string) => {
+    console.log('픽셀북 클릭:', bookId);
+    // TODO: 픽셀북 상세 화면으로 이동
+  };
+
+  // Empty 상태 체크
+  if (!hasBooks) {
+    return (
+      <EmptyStateLayout
+        floatingButton={<AddButton onPress={handleAddPicsel} />}>
+        <View className="flex-1">
+          <View className="absolute left-9 top-16">
+            {/* 추가하기 버튼만 표시 */}
+            <AddBookButton onPress={handleAddBook} />
+          </View>
+
+          {/* Empty 메시지 */}
+          <EmptyMessage
+            message="네컷사진을 모아두는 나만의 앨범,픽셀북을 추가해보세요!"
+            breakAtComma
+          />
+        </View>
+
+        <PicselBookBottomSheet ref={picselBookRef} onSubmit={handleSubmit} />
+      </EmptyStateLayout>
+    );
+  }
+
   return (
-    <EmptyStateLayout floatingButton={<AddButton onPress={handleAddPicsel} />}>
-      <View className="flex-1">
-        {/* 툴바 - 브랜드 필터 없이 */}
-        <PixelToolbar
-          totalPhotos={totalPhotos}
-          isSelecting={isSelecting}
-          selectedCount={selectedPhotos.length}
-          onToggleSelecting={() => setIsSelecting(!isSelecting)}
-          onSelectAll={() => selectAll(totalPhotos, photoData)}
-          onClose={resetSelection}
-          onSort={showSortSheet}
-        />
+    <View className="flex-1 bg-white">
+      {/* 툴바 - 브랜드 필터 없이 */}
+      <PixelToolbar
+        totalPhotos={totalBooks}
+        isSelecting={isSelecting}
+        selectedCount={selectedPhotos.length}
+        onToggleSelecting={() => setIsSelecting(!isSelecting)}
+        onSelectAll={() => selectAll(totalBooks, books)}
+        onClose={resetSelection}
+        onSort={showSortSheet}
+      />
 
-        <AddBookButton onPress={handleAddBook} />
+      {/* 픽셀북 리스트 (AddBookButton 포함) */}
+      <PicselBookList
+        books={books}
+        onBookPress={handleBookPress}
+        onAddBook={handleAddBook}
+      />
 
-        <PicselBookIcons shape="default" width={80} height={72} />
-        <PicselBookIcons shape="default" width={80} height={72} />
-        <PicselBookIcons shape="default" width={80} height={72} />
-        <PicselBookIcons shape="default" width={80} height={72} />
-
-        <EmptyMessage
-          message="네컷사진을 모아두는 나만의 앨범,픽셀북을 추가해보세요!"
-          breakAtComma
-        />
-      </View>
-
+      {/* 픽셀북 생성 바텀시트 */}
       <PicselBookBottomSheet ref={picselBookRef} onSubmit={handleSubmit} />
-    </EmptyStateLayout>
+
+      {/* 플로팅 버튼 */}
+      <View className="absolute -bottom-4 right-4">
+        <AddButton onPress={handleAddPicsel} />
+      </View>
+    </View>
   );
 };
 

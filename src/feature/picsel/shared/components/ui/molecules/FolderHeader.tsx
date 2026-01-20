a4ae0380efccb8d2415f682066b98ca631ad4271
hyeone999 <caps026@naver.com>
2026-01-20 17:14:54 +0900

    ✨ Feat : 픽셀북 header UI 분기처리

--- src/feature/picsel/shared/components/ui/molecules/FolderHeader.tsx
+++ src/feature/picsel/shared/components/ui/molecules/FolderHeader.tsx
@@ -2,22 +2,35 @@ import React from 'react';
 
 import { Pressable, Text, View } from 'react-native';
 
+import Toggle from '@/assets/icons/toggle/icon-toggle.svg';
 import ArrowIcons from '@/shared/icons/ArrowIcons';
 
 interface FolderHeaderProps {
   title: string;
   onBack: () => void;
+  showToggle?: boolean;
+  onTogglePress?: () => void;
 }
 
-const FolderHeader = ({ title, onBack }: FolderHeaderProps) => {
+const FolderHeader = ({
+  title,
+  onBack,
+  showToggle = false,
+  onTogglePress,
+}: FolderHeaderProps) => {
   return (
     <View className="flex-row items-center bg-white px-6 pt-4">
       <Pressable onPress={onBack}>
         <ArrowIcons shape="back" width={24} height={24} />
       </Pressable>
-      <Text className="flex-1 text-center text-gray-900 headline-03">
-        {title}
-      </Text>
+      <View className="flex-1 flex-row items-center justify-center">
+        <Text className="text-gray-900 headline-03">{title}</Text>
+        {showToggle && (
+          <Pressable onPress={onTogglePress}>
+            <Toggle />
+          </Pressable>
+        )}
+      </View>
       <View className="w-6" />
     </View>
   );

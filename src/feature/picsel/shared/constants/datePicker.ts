ab07fbdf149a7e11eb9ca536f560f5d73916c075
srchae <csulin77@gmail.com>
2026-01-28 19:36:46 +0900

    ✨ Feat :  datePicker 구현에 필요한 상수 선언

---
+++ src/feature/picsel/shared/constants/datePicker.ts
@@ -0,0 +1,24 @@
+export const WEEK_DAYS = [
+  { label: 'M', key: 'mon' },
+  { label: 'T', key: 'tue' },
+  { label: 'W', key: 'wed' },
+  { label: 'T', key: 'thu' },
+  { label: 'F', key: 'fri' },
+  { label: 'S', key: 'sat' },
+  { label: 'S', key: 'sun' },
+] as const;
+
+export const DAYS_PER_WEEK = 7;
+export const TOTAL_CALENDAR_CELLS = 42;
+export const CELL_WIDTH = `${100 / DAYS_PER_WEEK}%`;
+
+export const DATE_STYLES = {
+  SELECTED_BG: 'bg-pink-500',
+  TODAY_BORDER: 'border border-pink-300',
+  TEXT: {
+    DEFAULT: 'text-gray-900',
+    SELECTED: 'text-white headline-02',
+    TODAY: 'text-pink-500',
+    NOT_CURRENT_MONTH: 'text-gray-200',
+  },
+} as const;

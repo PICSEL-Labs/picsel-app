a33a3c56da07b8016991e6c8c39dbbb695bee08c
srchae <csulin77@gmail.com>
2026-01-28 19:41:13 +0900

    ✨ Feat :  사진 등록 화면 구조 편입에 따른 사진 등록 UI 구현

---
+++ src/feature/picsel/picselUpload/ui/organisms/PhotoSelectStep.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+
+import ExtraPhotoList from '@/feature/picsel/picselUpload/ui/organisms/ExtraPhotoList';
+import MainPhotoCard from '@/feature/picsel/picselUpload/ui/organisms/MainPhotoCard';
+import { RootStackNavigationProp } from '@/navigation/types/navigateTypeUtil';
+import { usePhotoStore } from '@/shared/store/picselUpload';
+import Button from '@/shared/ui/atoms/Button';
+
+interface Props {
+  onNext: () => void;
+}
+const PhotoSelectStep = ({ onNext }: Props) => {
+  const navigation = useNavigation<RootStackNavigationProp>();
+
+  const { mainPhoto, extraPhotos, setMainPhoto, removeExtraPhoto } =
+    usePhotoStore();
+
+  return (
+    <>
+      <View className="flex-1">
+        <View className="gap-2 px-5 pb-3 pt-5">
+          <Text className="text-gray-900 headline-04">
+            저장할 <Text className="text-pink-500 title-02">사진</Text>을
+            등록해주세요
+          </Text>
+          <Text className="text-gray-600 body-rg-02">
+            네컷 사진과 함께 추억이 담긴 {'\n'}다른 사진도 추가해 저장할 수
+            있어요.
+          </Text>
+        </View>
+        <MainPhotoCard
+          uri={mainPhoto}
+          onDelete={() => setMainPhoto(null)}
+          onSelect={() => {
+            navigation.navigate('SelectMainPhoto');
+          }}
+        />
+        <ExtraPhotoList
+          photos={extraPhotos}
+          onAdd={() => navigation.navigate('SelectExtraPhoto')}
+          onRemove={removeExtraPhoto}
+        />
+        <View className="absolute bottom-2 w-full items-center">
+          <Button
+            text="다음"
+            color={`${mainPhoto ? 'active' : 'disabled'}`}
+            textColor="white"
+            disabled={!mainPhoto}
+            onPress={onNext}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default PhotoSelectStep;

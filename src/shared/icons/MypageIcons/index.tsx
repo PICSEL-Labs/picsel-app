8fa45dbd12b4e31de59de49fa13593fc18f8240a
hyeone999 <caps026@naver.com>
2026-01-29 11:27:44 +0900

    ✨ Feat :  마이페이지-메인 아이콘 컴포넌트 추가

--- src/shared/icons/MypageIcons/index.tsx
+++ src/shared/icons/MypageIcons/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Announcement from '@/assets/icons/mypage/icon-announcement.svg';
 import Edit from '@/assets/icons/mypage/icon-edit-name.svg';
+import Help from '@/assets/icons/mypage/icon-help.svg';
 import Setting from '@/assets/icons/mypage/icon-setting.svg';
+import Star from '@/assets/icons/mypage/icon-star.svg';
 
 interface Props {
-  shape: 'setting' | 'edit-name';
+  shape: 'setting' | 'edit-name' | 'announcement' | 'help' | 'star';
   width: number;
   height: number;
 }
@@ -15,6 +18,12 @@ const MypageIcons = ({ shape, width, height }: Props) => {
       return <Setting width={width} height={height} />;
     case 'edit-name':
       return <Edit width={width} height={height} />;
+    case 'announcement':
+      return <Announcement width={width} height={height} />;
+    case 'help':
+      return <Help width={width} height={height} />;
+    case 'star':
+      return <Star width={width} height={height} />;
     default:
       return <Setting width={width} height={height} />;
   }

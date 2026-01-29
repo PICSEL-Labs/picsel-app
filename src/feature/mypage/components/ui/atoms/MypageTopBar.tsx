d91d91c98afdb6a540096c2393ec4140fd36f8a3
hyeone999 <caps026@naver.com>
2026-01-29 11:32:36 +0900

    ✨ Feat : 알림 관련 주석 추가

--- src/feature/mypage/components/ui/atoms/MypageTopBar.tsx
+++ src/feature/mypage/components/ui/atoms/MypageTopBar.tsx
@@ -16,6 +16,7 @@ const MypageTopBar = ({ onPressNotification, onPressSetting }: Props) => {
       className="flex h-11 w-full flex-row items-center justify-end px-4"
       style={{ gap: 16 }}>
       <Pressable onPress={onPressNotification}>
+        {/* 알림 여부를 기반으로 on - off 설정 -> 기획 및 기능 명세 필요 */}
         <BellIcons shape="on" width={24} height={24} />
       </Pressable>
 

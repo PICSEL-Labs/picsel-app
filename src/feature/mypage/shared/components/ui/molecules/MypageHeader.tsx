6444a0aacd4af477df540dbb0fcb7e10630d54e2
hyeone999 <caps026@naver.com>
2026-02-01 11:40:10 +0900

    ✨ Feat : 마이페이지[공통] 헤더 컴포넌트 구현

---
+++ src/feature/mypage/shared/components/ui/molecules/MypageHeader.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Pressable } from 'react-native';
+
+import ArrowIcons from '@/shared/icons/ArrowIcons';
+
+interface Props {
+  title: string;
+  showBackButton?: boolean;
+  rightElement?: ReactNode;
+  className?: string;
+}
+
+const MypageHeader = ({
+  title,
+  showBackButton = true,
+  rightElement,
+  className = '',
+}: Props) => {
+  const navigation = useNavigation();
+
+  return (
+    <View className={`flex w-full flex-row items-center py-2 ${className}`}>
+      {showBackButton && (
+        <Pressable
+          className="absolute left-4 z-10"
+          onPress={() => navigation.goBack()}>
+          <ArrowIcons shape="back" width={24} height={24} />
+        </Pressable>
+      )}
+
+      <Text className="flex-1 text-center text-gray-900 title-01">{title}</Text>
+
+      {rightElement && (
+        <View className="absolute right-4 z-10">{rightElement}</View>
+      )}
+    </View>
+  );
+};
+
+export default MypageHeader;

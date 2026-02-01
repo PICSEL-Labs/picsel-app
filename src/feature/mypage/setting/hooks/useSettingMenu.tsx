fada113c8fc8f3da65fcb2bd83dbfe9b8666a95f
hyeone999 <caps026@naver.com>
2026-02-01 11:41:29 +0900

    ✨ Feat : 마이페이지-설정 컴포넌트 및 hooks 구현

---
+++ src/feature/mypage/setting/hooks/useSettingMenu.tsx
@@ -0,0 +1,80 @@
+// @/feature/setting/hooks/useSettingMenu.tsx
+import { useCallback } from 'react';
+import React from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+import { SETTING_MENU_ITEMS } from '../constants/settingMenuItems';
+
+import { RootStackNavigationProp } from '@/navigation/types/navigateTypeUtil';
+import ArrowIcons from '@/shared/icons/ArrowIcons';
+import BellIcons from '@/shared/icons/BellIcons';
+import MypageIcons from '@/shared/icons/MypageIcons';
+
+const getIconByType = (iconType?: string) => {
+  switch (iconType) {
+    case 'account':
+      return <MypageIcons shape="my" width={24} height={24} />;
+    case 'notification':
+      return <BellIcons shape="off" width={24} height={24} />;
+    case 'terms':
+      return <MypageIcons shape="file" width={24} height={24} />;
+    case 'privacy':
+      return <MypageIcons shape="info-circle" width={24} height={24} />;
+    default:
+      return null;
+  }
+};
+
+export const useSettingMenu = () => {
+  const navigation = useNavigation<RootStackNavigationProp>();
+
+  const handleAccountManage = useCallback(() => {
+    navigation.navigate('MypageAccount');
+  }, []);
+
+  const handleNotificationSetting = useCallback(() => {
+    console.log('알림 설정');
+    // navigation.navigate('NotificationSetting');
+  }, []);
+
+  const handleTerms = useCallback(() => {
+    console.log('이용약관');
+    // navigation.navigate('Terms');
+  }, []);
+
+  const handlePrivacy = useCallback(() => {
+    console.log('개인정보처리방침');
+    // navigation.navigate('Privacy');
+  }, []);
+
+  const menuItems = SETTING_MENU_ITEMS.map(item => {
+    let onPress = () => {};
+
+    switch (item.id) {
+      case '1':
+        onPress = handleAccountManage;
+        break;
+      case '2':
+        onPress = handleNotificationSetting;
+        break;
+      case '3':
+        onPress = handleTerms;
+        break;
+      case '4':
+        onPress = handlePrivacy;
+        break;
+    }
+
+    return {
+      ...item,
+      onPress,
+      leftIcon: getIconByType(item.iconType),
+      rightIcon: item.hasRightArrow ? (
+        <ArrowIcons shape="next" width={24} height={24} />
+      ) : null,
+    };
+  });
+
+  return { menuItems };
+};

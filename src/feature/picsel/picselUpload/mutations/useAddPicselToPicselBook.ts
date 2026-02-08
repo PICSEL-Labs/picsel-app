3f9344b0b64fae6b3cb41f9116f1b670da2cdd63
srchae <csulin77@gmail.com>
2026-02-09 00:53:55 +0900

    ✨ Feat :  픽셀 업로드 API hook 생성

---
+++ src/feature/picsel/picselUpload/mutations/useAddPicselToPicselBook.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { addPicselToPicselBookApi } from '../api/addPicselToPicselBookApi';
+import { PicselUploadRequest } from '../types';
+
+import { uploadMultipleImagesToS3 } from '@/shared/utils/imageUpload';
+
+export const useAddPicselToPicselBook = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (request: PicselUploadRequest) => {
+      const s3ImageUrls = await uploadMultipleImagesToS3(
+        request.imagePaths,
+        'PICSELBOOK',
+      );
+
+      return addPicselToPicselBookApi({
+        ...request,
+        imagePaths: s3ImageUrls,
+      });
+    },
+    onSuccess: response => {
+      console.log('픽셀 업로드 성공:', response);
+      queryClient.invalidateQueries({ queryKey: ['picselBooks'] });
+    },
+    onError: error => {
+      console.log('픽셀 업로드 실패:', error);
+    },
+  });
+};

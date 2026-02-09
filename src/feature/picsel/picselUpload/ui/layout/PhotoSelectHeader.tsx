6f7e1e44c29e6c2d35900b806e2bbf4294f0c53b
srchae <csulin77@gmail.com>
2026-02-09 22:16:00 +0900

    ✨ Feat :  별도 분리한 헤더 텍스트 함수 적용

--- src/feature/picsel/picselUpload/ui/layout/PhotoSelectHeader.tsx
+++ src/feature/picsel/picselUpload/ui/layout/PhotoSelectHeader.tsx
@@ -3,55 +3,21 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Pressable, Text, View } from 'react-native';
 
+import { getPhotoHeaderText, PhotoVariant } from '../../lib/getPhotoHeaderText';
+
 import { RootStackNavigationProp } from '@/navigation/types/navigateTypeUtil';
 import ArrowIcons from '@/shared/icons/ArrowIcons';
 import ReplayIcons from '@/shared/icons/ReplayIcon';
 
 interface Props {
-  variant: 'main' | 'extra' | 'cover'; // 대표사진 / 추가사진 / 픽셀북 커버 사진
+  variant: PhotoVariant;
   onReset?: () => void;
   hasSelected?: boolean;
 }
 
 const PhotoSelectHeader = ({ variant, onReset, hasSelected }: Props) => {
   const navigation = useNavigation<RootStackNavigationProp>();
-
-  const getHeaderText = () => {
-    switch (variant) {
-      case 'cover':
-        return {
-          title: '사진 선택',
-          desc: (
-            <Text className="text-center text-gray-500 headline-01">
-              커버사진으로 지정할{' '}
-              <Text className="text-pink-500">사진 1장</Text>을 골라주세요.
-            </Text>
-          ),
-        };
-      case 'main':
-        return {
-          title: '사진 등록',
-          desc: (
-            <Text className="text-center text-gray-500 headline-01">
-              대표사진으로 지정할{' '}
-              <Text className="text-pink-500">네컷사진 1장</Text>을 골라주세요.
-            </Text>
-          ),
-        };
-      case 'extra':
-        return {
-          title: '사진 등록',
-          desc: (
-            <Text className="text-center text-gray-500 headline-01">
-              추가사진은 <Text className="text-pink-500">최대 10장</Text>까지
-              선택 가능해요.
-            </Text>
-          ),
-        };
-    }
-  };
-
-  const content = getHeaderText();
+  const { title, desc } = getPhotoHeaderText(variant);
 
   return (
     <>
@@ -61,9 +27,7 @@ const PhotoSelectHeader = ({ variant, onReset, hasSelected }: Props) => {
           className="absolute left-5 items-start">
           <ArrowIcons shape="back" width={24} height={24} />
         </Pressable>
-        <Text className="text-center text-gray-900 title-01">
-          {content.title}
-        </Text>
+        <Text className="text-center text-gray-900 title-01">{title}</Text>
         {hasSelected && (
           <Pressable
             onPress={onReset}
@@ -74,7 +38,7 @@ const PhotoSelectHeader = ({ variant, onReset, hasSelected }: Props) => {
         )}
       </View>
 
-      <View className="px-1 py-2">{content.desc}</View>
+      <View className="px-1 py-2">{desc}</View>
     </>
   );
 };

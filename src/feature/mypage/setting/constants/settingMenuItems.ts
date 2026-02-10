5bd67c9caaa9240f427d23fe0e0e29e546127b20
hyeone999 <caps026@naver.com>
2026-02-10 12:28:34 +0900

    ✨ Feat : 링킹 상수로 분리

--- src/feature/mypage/setting/constants/settingMenuItems.ts
+++ src/feature/mypage/setting/constants/settingMenuItems.ts
@@ -1,3 +1,10 @@
+export const EXTERNAL_LINKS = {
+  terms:
+    'https://daisy-tamarillo-7ee.notion.site/2152a4516dff8001bcc4c7fb6a3229da',
+  privacy:
+    'https://daisy-tamarillo-7ee.notion.site/2672a4516dff801d9ee1d7edbdfa3323',
+} as const;
+
 export interface SettingMenuItem {
   id: string;
   label: string;
--- src/feature/mypage/setting/hooks/useSettingMenu.tsx
+++ src/feature/mypage/setting/hooks/useSettingMenu.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Linking } from 'react-native';
 
-import { SETTING_MENU_ITEMS } from '../constants/settingMenuItems';
+import {
+  EXTERNAL_LINKS,
+  SETTING_MENU_ITEMS,
+} from '../constants/settingMenuItems';
 
 import { RootStackNavigationProp } from '@/navigation/types/navigateTypeUtil';
 import ArrowIcons from '@/shared/icons/ArrowIcons';
@@ -38,15 +41,11 @@ export const useSettingMenu = () => {
   }, []);
 
   const handleTerms = useCallback(() => {
-    Linking.openURL(
-      'https://daisy-tamarillo-7ee.notion.site/2152a4516dff8001bcc4c7fb6a3229da',
-    );
+    Linking.openURL(EXTERNAL_LINKS.terms);
   }, []);
 
   const handlePrivacy = useCallback(() => {
-    Linking.openURL(
-      'https://daisy-tamarillo-7ee.notion.site/2672a4516dff801d9ee1d7edbdfa3323',
-    );
+    Linking.openURL(EXTERNAL_LINKS.privacy);
   }, []);
 
   const menuItems = SETTING_MENU_ITEMS.map(item => {

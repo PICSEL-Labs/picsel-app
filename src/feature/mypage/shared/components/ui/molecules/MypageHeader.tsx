f2afd9f5c41e2b7cfb27a2148957e8e1f17a9d68
hyeone999 <caps026@naver.com>
2026-02-11 12:18:16 +0900

    ✨ Feat : white mode 구현

--- src/feature/mypage/shared/components/ui/molecules/MypageHeader.tsx
+++ src/feature/mypage/shared/components/ui/molecules/MypageHeader.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
+import clsx from 'clsx';
 import { View, Text, Pressable } from 'react-native';
 
 import ArrowIcons from '@/shared/icons/ArrowIcons';
 
 interface Props {
   title: string;
+  white?: boolean;
   showBackButton?: boolean;
   rightElement?: ReactNode;
   className?: string;
@@ -17,6 +19,7 @@ interface Props {
 const MypageHeader = ({
   title,
   showBackButton = true,
+  white = false,
   rightElement,
   className = '',
   rightIconPress,
@@ -30,11 +33,21 @@ const MypageHeader = ({
         <Pressable
           className="absolute left-4 z-10"
           onPress={onBackPress ?? (() => navigation.goBack())}>
-          <ArrowIcons shape="back" width={24} height={24} />
+          <ArrowIcons
+            shape={white ? 'back-white' : 'back'}
+            width={24}
+            height={24}
+          />
         </Pressable>
       )}
 
-      <Text className="flex-1 text-center text-gray-900 title-01">{title}</Text>
+      <Text
+        className={clsx(
+          'flex-1 text-center text-gray-900 title-01',
+          white && 'text-white',
+        )}>
+        {title}
+      </Text>
 
       {rightElement && (
         <Pressable onPress={rightIconPress} className="absolute right-4 z-10">

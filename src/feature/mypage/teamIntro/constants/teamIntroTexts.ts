f65956caa40539966f130eb14c232a6461670f26
hyeone999 <caps026@naver.com>
2026-02-11 16:05:55 +0900

    ✨ Feat : 팀원 소개 페이지 구현

---
+++ src/feature/mypage/teamIntro/constants/teamIntroTexts.ts
@@ -0,0 +1,19 @@
+export const TEAM_INTRO_DESCRIPTION =
+  '안녕하세요.\n포토부스에서 찍은 특별한 순간을 기억하는 공간,\nPicsel을 제작한 개발팀입니다.\n\n앞으로의 Picsel의 꾸준한 성장을 응원해주세요💗';
+
+export const TEAM_MEMBERS = [
+  [
+    { role: 'Product Manager', name: '김경진' },
+    { role: 'Product Designer', name: '정다은' },
+  ],
+  [
+    { role: 'Backend Engineer', name: '조준희' },
+    { role: 'Frontend Engineer', name: '김현중  채수린' },
+  ],
+] as const;
+
+export const TEAM_LINKS = {
+  instagram: '',
+  donation: '',
+  introduce: '',
+} as const;

90b12c577f5f9c202d9e73e39ff21a4249f75aa1
srchae <csulin77@gmail.com>
2026-02-25 22:48:40 +0900

    ✨ Feat :  이미지 업로드 시 필요한 드래프트 uuid를 파라미터로 정의

--- src/shared/utils/imageUpload.ts
+++ src/shared/utils/imageUpload.ts
@@ -24,17 +24,20 @@ const resolveLocalFilePath = async (uri: string): Promise<string> => {
     return resolvedPath;
   } catch (error) {
     console.log('iOS 이미지 경로 변환 실패', error);
+    return uri;
   }
 };
 
 /**
  * 로컬 이미지 URI를 받아 S3 저장소에 업로드하고, 서버(DB) 저장에 필요한 최종 URL을 반환
  * @param {string} uri - 업로드할 로컬 이미지 URI
+ * @param uuid draftUuid - 이미지 업로드 시 서버에서 Presigned URL 발급을 위해 필요한 고유 식별자
  * @param {ImageType} type - 이미지 업로드 타입
  * @returns {Promise<string>} S3 업로드 완료 후 생성된 이미지의 최종 URL
  */
 export const uploadImageToS3 = async (
   uri: string,
+  uuid: string,
   type: ImageType,
 ): Promise<string> => {
   const localPath = await resolveLocalFilePath(uri);
@@ -42,7 +45,7 @@ export const uploadImageToS3 = async (
   // Presigned URL 획득
   const {
     data: { presignedUrl, fileUrl },
-  } = await getPresignedUrlApi(type);
+  } = await getPresignedUrlApi(uuid, type);
 
   // 파일 Fetch 및 Blob 변환
   const response = await fetch(localPath);
@@ -65,13 +68,15 @@ export const uploadImageToS3 = async (
 /**
  * 여러 장의 이미지 URI 배열을 받아 병렬로 S3에 업로드
  * @param {string[]} uris - 업로드할 로컬 이미지 URI 배열
+ * @param uuid draftUuid - 이미지 업로드 시 서버에서 Presigned URL 발급을 위해 필요한 고유 식별자
  * @param {ImageType} type - 이미지 업로드 타입
  * @returns {Promise<string[]>} 업로드된 모든 이미지의 최종 URL 배열
  * @description 모든 이미지는 Promise.all을 통해 동시에 업로드 시작
  */
 export const uploadMultipleImagesToS3 = async (
   uris: string[],
+  uuid: string,
   type: ImageType,
 ): Promise<string[]> => {
-  return Promise.all(uris.map(uri => uploadImageToS3(uri, type)));
+  return Promise.all(uris.map(uri => uploadImageToS3(uri, uuid, type)));
 };

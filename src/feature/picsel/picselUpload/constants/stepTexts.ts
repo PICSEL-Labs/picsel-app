b64cfe72086c8b7e10779dc1321cb764acfb5d2c
srchae <csulin77@gmail.com>
2026-03-11 23:14:14 +0900

    ✨ Feat :  픽셀 업로드 단계 내 정의되는 텍스트 상수화

---
+++ src/feature/picsel/picselUpload/constants/stepTexts.ts
@@ -0,0 +1,15 @@
+export const STEP_TEXTS = {
+  PHOTO_SELECT: {
+    DESCRIPTION:
+      '네컷 사진과 함께 추억이 담긴\n다른 사진도 추가해 저장할 수 있어요.',
+  },
+  DATE_LOCATION: {
+    DESCRIPTION: '촬영 일자와 장소는 필수 입력 정보에요.',
+  },
+  PICSEL_BOOK_SELECT: {
+    DESCRIPTION: '추억을 담을 픽셀북을 선택하거나\n새롭게 추가할 수 있어요.',
+  },
+  RECORD_WRITE: {
+    DESCRIPTION: '사진과 함께 이 날의 이야기를 간단히 남겨보세요!',
+  },
+} as const;

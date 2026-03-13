e5bb6e0820db386aa12d072c721a76856df51ccc
srchae <csulin77@gmail.com>
2026-03-13 23:23:30 +0900

    ✨ Feat :  기능 설계 이슈에 따른 ImageViewer의 dropdown menu 재구성

--- src/feature/picsel/myPicsel/hooks/usePhotoViewerMenu.tsx
+++ src/feature/picsel/myPicsel/hooks/usePhotoViewerMenu.tsx
@@ -1,88 +1,33 @@
 import React, { useMemo } from 'react';
 
-import { useNavigation } from '@react-navigation/native';
+import { DROPDOWN_ITEMS } from '../constants/photoViewerTexts';
 
-import {
-  DELETE_ALERT,
-  DROPDOWN_ITEMS,
-  SET_REPRESENTATIVE_ALERT,
-  TOAST_MESSAGES,
-} from '../constants/photoViewerTexts';
+import { useSavePhoto } from './useSavePhoto';
 
-import { RootStackNavigationProp } from '@/navigation/types/navigateTypeUtil';
 import { DropdownMenuItem } from '@/shared/components/ui/molecules/DropdownMenu';
 import PicselActionIcons from '@/shared/icons/PicselActionIcons';
-import PicselPhotoIcons from '@/shared/icons/PicselPhotoIcons';
-import { showConfirmModal } from '@/shared/lib/confirmModal';
-import { useToastStore } from '@/shared/store/ui/toast';
 
 interface Props {
-  picselId: string;
-  imagePath: string;
+  uri: string;
   hideDropdown: (onComplete?: () => void) => void;
 }
 
-export const usePhotoViewerMenu = ({
-  picselId,
-  imagePath,
-  hideDropdown,
-}: Props) => {
-  const navigation = useNavigation<RootStackNavigationProp>();
-  const { showToast } = useToastStore();
+export const usePhotoViewerMenu = ({ uri, hideDropdown }: Props) => {
+  const { savePhoto } = useSavePhoto(uri);
 
   const dropdownItems: DropdownMenuItem[] = useMemo(
     () => [
       {
-        label: DROPDOWN_ITEMS.SET_REPRESENTATIVE,
-        onPress: () => {
-          hideDropdown(() => {
-            showConfirmModal(
-              SET_REPRESENTATIVE_ALERT.DESCRIPTION,
-              () => {
-                console.log('대표사진으로 변경:', picselId);
-                showToast(TOAST_MESSAGES.REPRESENTATIVE_SUCCESS);
-              },
-              {
-                title: SET_REPRESENTATIVE_ALERT.TITLE,
-                confirmText: SET_REPRESENTATIVE_ALERT.CONFIRM,
-              },
-            );
-          });
-        },
-        icon: <PicselPhotoIcons shape="darkgray" height={24} width={24} />,
-      },
-      {
         label: DROPDOWN_ITEMS.SAVE_PHOTO,
         onPress: () => {
           hideDropdown(() => {
-            console.log('사진 저장:', imagePath);
+            savePhoto();
           });
         },
         icon: <PicselActionIcons shape="save" width={24} height={24} />,
       },
-      {
-        label: DROPDOWN_ITEMS.DELETE,
-        onPress: () => {
-          hideDropdown(() => {
-            showConfirmModal(
-              DELETE_ALERT.DESCRIPTION,
-              () => {
-                console.log('삭제');
-                showToast(TOAST_MESSAGES.DELETE_SUCCESS);
-                navigation.goBack();
-              },
-              {
-                title: DELETE_ALERT.TITLE,
-                confirmText: DELETE_ALERT.CONFIRM,
-              },
-            );
-          });
-        },
-        textClassName: 'text-semantic-error body-rg-04',
-        icon: <PicselActionIcons shape="delete" width={24} height={24} />,
-      },
     ],
-    [hideDropdown, picselId, showToast, navigation, imagePath],
+    [hideDropdown, savePhoto],
   );
 
   return { dropdownItems };

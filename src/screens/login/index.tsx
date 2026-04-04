4afc5d4bd77bf6f5ca0850a54928151749e80f2f
hyeone999 <caps026@naver.com>
2026-04-04 21:55:56 +0900

    ✨ Feat :  회원탈퇴 후 로그인 화면 안내 모달 추가

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 import { View } from 'react-native';
@@ -8,11 +8,27 @@ import { SocialTypes } from '@/feature/auth/login/types';
 import LoginIntro from '@/feature/auth/login/ui/organisms/LoginIntro';
 import { SignupNavigationProp } from '@/navigation/types/navigateTypeUtil';
 import ScreenLayout from '@/shared/components/layouts/ScreenLayout';
+import { useConfirmModalStore } from '@/shared/store/ui/confirmModal';
+import { useUserStore } from '@/shared/store/user';
 import SocialLoginButton from '@/shared/ui/molecules/SocialLoginButton';
 
 const LoginScreen = () => {
   const navigation = useNavigation<SignupNavigationProp>();
   const { handleSocialLogin } = useLoginService(navigation);
+  const { isWithdrawn, setIsWithdrawn } = useUserStore();
+
+  useEffect(() => {
+    if (isWithdrawn) {
+      useConfirmModalStore.getState().showConfirmModal({
+        title: '잠시만 기다려 주세요',
+        message:
+          '아직 회원 탈퇴 처리 중이에요🥹\n다음 날 새벽 2시 이후 재가입이 가능해요.',
+        confirmText: '확인',
+        isSingleButton: true,
+        onConfirm: () => setIsWithdrawn(false),
+      });
+    }
+  }, [isWithdrawn, setIsWithdrawn]);
 
   return (
     <ScreenLayout className="bg-primary-black">

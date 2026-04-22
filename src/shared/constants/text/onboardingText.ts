bf7d0514e26b30c5133fd0524cc63db9142db679
hyeone999 <caps026@naver.com>
2026-04-22 20:40:49 +0900

    ✨ Feat :  변경된 온보딩 이미지 적용

--- src/shared/constants/text/onboardingText.ts
+++ src/shared/constants/text/onboardingText.ts
@@ -1,26 +1,18 @@
 export const ONBOARDING_SLIDES = [
   {
     id: 0,
-    title: '내 취향대로 탐색하는',
-    highlight: '내 주변 포토부스',
     image: require('@/assets/images/onboarding/onboarding-1.png'),
   },
   {
     id: 1,
-    title: 'QR 스캔 한번으로',
-    highlight: '빠르고 간편한 업로드',
     image: require('@/assets/images/onboarding/onboarding-2.png'),
   },
   {
     id: 2,
-    title: '날짜와 장소를 함께 기록하는',
-    highlight: '나만의 네컷 보관함',
     image: require('@/assets/images/onboarding/onboarding-3.png'),
   },
   {
     id: 3,
-    title: '흩어져있던 네컷사진,',
-    highlight: '나만의 앨범으로',
     image: require('@/assets/images/onboarding/onboarding-4.png'),
   },
 ];
